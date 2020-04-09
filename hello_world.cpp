34174e39c534e4577cad0c5bca7756be4ebcd4c4
vincentlaguna <vincentlaguna@live.com>
2020-04-09 19:44:36 +0000

    Hello World!

---
+++ hello_world.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() 
+{
+  
+  std::cout << "Hello World!\n";
+  std::cout << "My goal is to learn C++ proficiently\n";
+  std::cout << "Vincent Laguna 4-8-2020\n";
+
+}
