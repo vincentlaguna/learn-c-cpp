1dd7e9599903c2d5d0eb0fe81802ce18c6c35384
vincentlaguna <vincentlaguna@live.com>
2020-04-22 03:32:02 +0000

    update sorting_hat.cpp

--- sorting_hat.cpp
+++ sorting_hat.cpp
@@ -14,5 +14,15 @@ int main() {
   std::cout << "||=======================||\n";
   std::cout << "|| The Sorting Hat quiz! ||\n";
   std::cout << "||=======================||\n\n";
-  std::cout << "Q1) When I'm dead, I want people to remember me as...\n";
+  std::cout << "Q1) When I'm dead, I want people to remember me as: \n\n";
+  std::cout << "1) The Good\n";
+  std::cout << "2) The Great\n";
+  std::cout << "3) The Wise\n";
+  std::cout << "The Bold\n";
+  std::cin >> answer1;
+  
+  if (answer1 == 1) {
+    hufflepuff += 1;
+  }
+  else if (answer1 == 2)
 }
