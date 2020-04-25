20b6fb1528b135877f70a92fc48debb8b861dea3
vincentlaguna <vincentlaguna@live.com>
2020-04-25 21:23:35 +0000

    leap year in C

---
+++ c/leap_year.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  int year;
+  
+  printf("Please enter year to check if it is a leap year: ");
+  scanf("%d", &year);
+  
+  if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+    printf("%d is a leap year\n", year);
+  }
+  else {
+    printf("%d is not a leap year\n", year);
+  }
+}
