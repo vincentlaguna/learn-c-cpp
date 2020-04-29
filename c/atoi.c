7f454ab44acc21d8d34d915fa4f9e9bf8448531a
vincentlaguna <vincentlaguna@live.com>
2020-04-29 20:03:58 +0000

    htoi in C

--- c/atoi.c
+++ c/atoi.c
@@ -1,14 +1,20 @@
-/* atoi: convert s to integer */
+// needs debugging...
+
 #include <stdio.h>
 
 int main()
 {
-int atoi(char s[]) {
+  char s[] = "28s";
+  
+  printf("The string '%s' converted to int: %d\n", s, a_toi(s));
+  return 0;
+}
+// atoi: convert s to integer
+int a_toi(char s[])
+{
   int i, n;
   n = 0;
-  
   for (i = 0; s[i] >= '0' && s[i] <= '9'; ++i)
     n = 10 * n + (s[i] - '0');
     return n;
-  }
 }
