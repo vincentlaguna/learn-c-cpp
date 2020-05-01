dc5131899fa7821d43204c2cb23194ffefece726
vincentlaguna <vincentlaguna@live.com>
2020-05-01 04:39:27 +0000

    add in progress - C

---
+++ c/simple_math/math.c
@@ -0,0 +1,12 @@
+
+int math_add(int n1, int n2);
+int math_sub(int n1, int n2);
+long long int math_mul(int n1, int n2);
+float math_div(int n1, int n2);
+
+void function_add_numbers(int a, int b, int c) 
+{
+  int sum;
+  sum = a + b + c;
+  printf("Sum = %d\n", sum);
+}
---
+++ c/simple_math/math.h
@@ -0,0 +1,9 @@
+#ifndef _MATH_H_
+#define _MATH_H_
+// These are also called exposed
+int math_add(int n1, int n2);
+int math_sub(int n1, int n2);
+long long int math_mul(int n1, int n2);
+float math_div(int n1, int n2);
+
+#endif
