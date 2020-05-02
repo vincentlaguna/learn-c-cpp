9286ca7a182fd4fa321ed6df2f751cb9b209b971
vincentlaguna <vincentlaguna@live.com>
2020-05-02 20:43:46 +0000

    vectors in C++

--- c++/vectors.cpp
+++ c++/vectors.cpp
@@ -14,5 +14,17 @@ int main() {
   std::cout << grocery[1] << "\n";
   std::cout << grocery[2] << "\n";
   std::cout << grocery[3] << "\n";
+  // Different examaple:
+  std::vector<double> delivery_order;
+  delivery_order.push_back(8.99);
+  delivery_order.push_back(3.75);
+  delivery_order.push_back(0.99);
+  delivery_order.push_back(5.99);
+  // Calculate the total using a for loop:
+  double total = 0.0;
+  for (int i = 0; i < delivery_order.size(); i++) {
+    total = total + delivery_order[i];
+  }
+  std::cout << total << "\n";
   
 }
