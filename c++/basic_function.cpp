4eebadd303e53c32d0a5043918a3d2639852f88a
vincentlaguna <vincentlaguna@live.com>
2020-05-05 19:58:48 +0000

    more basic functions in C++

--- c++/basic_function.cpp
+++ c++/basic_function.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 
-// Change needs_it_support so that it returns support:
+// random functions put together for examples...
 bool needs_it_support() {
-  
   bool support;
-  
   std::cout << "Hello. IT. Have you tried turning it off and on again? Enter 1 for yes, 0 for no.\n";
   std::cin >> support;
   return support;
-  
+}
+
+void name_x_times(std::string name, int x) {
+  while (x > 0) {
+    std::cout << name << "\n";
+    x--;
+  }
 }
 
 int main() {
-  
-  // Change the following line to print the function result:
   std::cout << needs_it_support() << "\n";
-  
+  std::string my_name = "Name = NULL";
+  int some_number = 5;
+  name_x_times(my_name, some_number);
 }
