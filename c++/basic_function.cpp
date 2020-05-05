e1633ee318df2b67a81d0302ac4050d29909b3aa
vincentlaguna <vincentlaguna@live.com>
2020-05-05 20:05:58 +0000

    some more basic functions in C++

--- c++/basic_function.cpp
+++ c++/basic_function.cpp
@@ -16,8 +16,21 @@ void name_x_times(std::string name, int x) {
 }
 
 int main() {
+  std::string on_off_attempt;
   std::cout << needs_it_support() << "\n";
-  std::string my_name = "Name = NULL";
+   // Check in with Jenn
+  std::cout << "Oh hi Jen!\n";
+  // Conduct IT support again...
+  std::cout << "Hello. IT.\n";
+  std::cout << "Have you tried turning it off and on again? y/n\n";
+  std::cin >> on_off_attempt;
+  // Check in with Roy
+  std::cout << "You stole the stress machine? But that's stealing!\n";
+  // Conduct IT support yet again...zzzz...
+  std::cout << "Hello. IT.\n";
+  std::cout << "Have you tried turning it off and on again? y/n\n";
+  std::cin >> on_off_attempt;
+  std::string my_name = "Name = NULL...404 NOT FOUND...";
   int some_number = 5;
   name_x_times(my_name, some_number);
 }
