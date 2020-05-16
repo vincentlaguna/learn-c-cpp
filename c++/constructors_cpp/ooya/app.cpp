7121da4f5902d504e2d7eb7f45a27319ff1e82cd
vincentlaguna <vincentlaguna@live.com>
2020-05-16 15:35:19 +0000

    ooya constructor declaration - C++

--- c++/constructors_cpp/ooya/app.cpp
+++ c++/constructors_cpp/ooya/app.cpp
@@ -2,5 +2,6 @@
 #include "profile.hpp"
 
 int main() {
-  
+  Profile tyler;
+  tyler.name = "Tyler Durden";
 }
--- c++/constructors_cpp/ooya/profile.cpp
+++ c++/constructors_cpp/ooya/profile.cpp
@@ -2,10 +2,14 @@
 #include <vector>
 
 class Profile {
+private:
   std::string name;
   int age;
   std::string city;
   std::string country;
   std::string pronouns;
   std::vector<std::string> hobbies;
+public:
+  Profile(std::string name, int age, std::string city, std::string country, 
+  std::string pronouns, std::vector<std::string> hobbies);
 };
