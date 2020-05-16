f87c3d92886873291030a1ece23d55f0db1076f1
vincentlaguna <vincentlaguna@live.com>
2020-05-16 15:23:24 +0000

    ooya project (codecademy) 1st commit - C++

---
+++ c++/constructors_cpp/ooya/app.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "profile.hpp"
+
+int main() {
+  
+}
---
+++ c++/constructors_cpp/ooya/profile.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+class Profile {
+  std::string name;
+  int age;
+  std::string city;
+  std::string country;
+  std::string pronouns;
+  std::vector<std::string> hobbies;
+};
