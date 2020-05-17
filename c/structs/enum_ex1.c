c5b66bba96c008a1f07a11c3b3abaf3086ce3fe7
vincentlaguna <vincentlaguna@live.com>
2020-05-17 21:32:12 +0000

    enum example updated - C

--- c/structs/enum_ex1.c
+++ c/structs/enum_ex1.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 
+typedef union
+{
+  int Integer;
+  char Character;
+} OneThingOrAnother;
 
+typedef enum 
+{
+  TheInteger = 10,
+  TheCharacter = 20
+} WhichThing;
 
 int main(void)
 {
+  OneThingOrAnother var;
+  var.Integer = 123;
+  
+  WhichThing type = TheInteger;
+  
+  printf("var [%d] type = [%d]\n", var.Integer, type);
+  
+  var.Character = 'V';
+  type = TheCharacter;
+  
+  printf("var [%c] type = [%d]\n", var.Character, type);
   
 }
