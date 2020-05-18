57031bf13a020a76e009ca5bba4a2737494f542f
vincentlaguna <vincentlaguna@live.com>
2020-05-18 20:00:25 +0000

    update bleep exercise - C++

---
+++ c++/functions/bleep/bleep.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include "functions.hpp"
+
+int main() {
+  std::string word = "broccoli";
+  std::string text = "I sometimes eat broccoli. There are three interesting things about broccoli. Number One. Nobody knows how to spell it. Number Two. No matter how long you boil it, it's always cold by the time it reaches your plate. Number Three. It's green. #broccoli. Rolling up in the broccoli eating a lot of broccoli, dreaming about broccoli.";
+}
