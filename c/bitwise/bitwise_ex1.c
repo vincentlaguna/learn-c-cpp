a82ca497e017324fccf5c70a50a91dfe5e577381
vincentlaguna <vincentlaguna@live.com>
2020-05-24 18:38:58 +0000

    update bitwise operators in C

--- c/bitwise/bitwise_ex1.c
+++ c/bitwise/bitwise_ex1.c
@@ -6,8 +6,18 @@ int main(void)
   unsigned int b = 13; // 0000 1101
   int result = 0;
   
-  result = a & b;
-  // 0000 1100
+  //result = a & b;
+  // 0000 1100 = 12
+  //result = a | b;
+  // 0011 1101 = 61
+  //result = a ^ b;
+  // 0011 0001 = 49
+  //result = ~a;
+  // 1100 0011 = -61
+  //result = a << 4;
+  // 0011 1100 0000 = 960
+  result = a >> 4;
+  // 0000 0011 = 3 (bit loss)
   printf("result is : [%d]\n", result);
   return 0;
 }
