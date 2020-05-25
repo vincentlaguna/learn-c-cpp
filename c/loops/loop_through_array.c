43955baaaaf3ca618b78e9e49d8ae3691da154f7
vincentlaguna <vincentlaguna@live.com>
2020-05-25 03:08:16 +0000

    loop through an array using pointers -C

--- c/loops/loop_through_array.c
+++ c/loops/loop_through_array.c
@@ -4,10 +4,11 @@ int main(void)
 {
   int numbers[3] = {101, 102, 103};
   int size = sizeof(numbers) / sizeof(numbers[0]);
+  int *p = numbers;
   
   for(int i = 0; i < size; i++)
   {
-    printf("[%d]\n", numbers[i]);
+    printf("[%d]\n", p[i]);
   }
   return 0;
 }
