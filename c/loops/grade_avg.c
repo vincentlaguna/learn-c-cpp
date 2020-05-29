2f6a4c31c7478f771f5264abffbe625142c723d7
vincentlaguna <vincentlaguna@live.com>
2020-05-29 15:24:27 +0000

    averaging ten numbers in an array based on user input - C

--- c/loops/grade_avg.c
+++ c/loops/grade_avg.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(void)
 {
   int numbers[10];
-  int sum = 0;
-  int count;
-  int avg = (sum / count);
+  long sum = 0;
+  int count = 10;
+  float avg = 0.0f;
   
-  printf("Please enter [10] grades to average:\n");
+  printf("Please enter [10] numbers to average:\n");
   
-  for(int count = 0; count < 10; count++)
+  for(int i = 0; i < count; ++i)
   {
-    printf("Enter grade %d>  ", count);
-    scanf("%d", &sum);
-    sum += sum;
+    printf("Enter number %2u>  ", i + 1);
+    scanf("%d", &numbers[i]);
+    sum += numbers[i];
   }
-  printf("Average is = [%d]\n", avg);
+  avg = (float)sum / count;
+  printf("The Average of the [10] numbers is = [%.2f]\n", avg);
   return 0;
 }
