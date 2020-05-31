080d44f94ea75f1a794a362bf19a4667259a405d
vincentlaguna <vincentlaguna@live.com>
2020-05-31 19:15:02 +0000

    rainfall averages update in C

--- c/loops/rainfall_averages.c
+++ c/loops/rainfall_averages.c
@@ -6,27 +6,35 @@ rainfall, and the average rainfall for each month.
 #define YEARS 5
 #define MONTHS 12
 
-float averages[[YEARS][MONTHS]] = 
-{
-  {,,,,,,,,,,,},
-  {,,,,,,,,,,,},
-  {,,,,,,,,,,,},
-  {,,,,,,,,,,,},
-  {,,,,,,,,,,,},
-}
-
 int main(void)
 { 
-  
-  for() // Totals for each year
+  // Initialize rainfall data for 2011-2015
+  float rain[YEARS][MONTHS] = 
   {
-    for() // Totals for month
-    {
-      
-    }
-    total // Total rainfall for all years
-  }
-  printf("YEAR\tRAINFALL (inches)\n");
+    {4.3,4.3,4.3,3.0,2.0,1.2,0.2,0.2,0.4,2.4,3.5,6.5},
+    {8.5,8.2,1.2,1.6,2.4,0.0,5.2,0.9,0.3,0.9,1.4,7.3},
+    {9.1,8.5,6.7,4.3,2.1,0.8,0.2,0.2,1.1,2.3,6.1,8.4},
+    {7.2,9.9,8.4,3.3,1.2,0.8,0.4,0.0,0.6,1.7,4.3,6.2},
+    {7.6,5.6,3.8,2.8,3.8,0.2,0.0,0.0,0.0,1.3,6.3,5.2},
+  };
+  // Variable declaration
+  int year;
+  int month;
+  float subTotal;
+  float total;
+  
+  printf("YEAR\t\tRAINFALL (inches)\n");
+  
+  for(year = 0, total = 0; year < YEARS; year++) // Totals for each year
+   {
+     for(month = 0, subTotal = 0; month < MONTHS; month++)
+     {
+       subTotal += rain[year][month];
+     }
+     printf("[%5d] %15.1f\n", 2010 + year, subTotal);
+   }
+  
+  /*
   printf("[%d]\t[%f]\n", 2010 + 1, avgyr);
   printf("The yearly average is [%f] inches.\n", yearsavg);
   printf("MMONTHLY AVERAGES:\n");
@@ -35,5 +43,6 @@ int main(void)
   printf("The average rainfall for [%d] is: [%f]\n", year, avgfloat);
   printf("The average rainfall for the month of [%s] is: [%f]\n", month, monthavg);
   printf("The total rainfall for the year [%d] is: [%f]\n", year, total);
+  */
   return 0;
 }
