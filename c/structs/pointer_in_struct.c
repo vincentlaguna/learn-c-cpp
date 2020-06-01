5d931a60fd6d8243c46b6b99434b0397d6d1d45d
vincentlaguna <vincentlaguna@live.com>
2020-06-01 20:50:35 +0000

    structure pointers update alternative method of accessing struct members -C

--- c/structs/pointer_in_struct.c
+++ c/structs/pointer_in_struct.c
@@ -19,3 +19,28 @@ int main(void)
   printf("Age of the student -> %d\n", stu1_ptr -> age);
   return 0;
 }
+
+/* Alternative way to access members of a structure using pointers:
+
+#include<stdio.h>  
+
+struct student  
+{  
+  char name[20];  
+  int class;  
+  int age;
+};  
+
+struct student stu1 = {"Tanuman",12,16};  
+struct student *stu2;  
+
+int main()  
+{
+  stu2 = &stu1; // g Structure variable to another 
+  printf("\n Name of student %s ",(*stu2).name);  
+  printf("\n Class of the student %d",(*stu2).class);  
+  printf("\n Age of the student %d ",(*stu2).age);
+  return 0;
+}
+
+*/
