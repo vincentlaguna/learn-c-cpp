5bd87ba7bccc644551c4cdea50722e4adc23960a
vincentlaguna <vincentlaguna@live.com>
2020-06-04 04:30:21 +0000

    tictactoe game - initial commit

---
+++ c/personal_projects/tictactoe_v1.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char square[10] = {'o', '1', '2', '3', '4','5', '6', '7', '8', '9'};
+int choice;
+int player;
+
+int checkForWin();
+void displayBoard();
+void markBoard(char mark);
+
+int main(void)
+{ 
+  
+  return 0;
+}
+
+int checkForWin()
+{ 
+  int returnValue = 0;
+  
+  if(square[1] == square[2] && square[2] == square[3])
+  {
+    returnValue = 1; 
+  }
+  else if(square[1] == square[5] && square[5] == square[9])
+  {
+    returnValue = 1;
+  }
+  else if(square[1] == square[4] && square[4] == square[7])
+  {
+    returnValue = 1;
+  }
+  else if(square[4] == square[5] && square[5] == square[6])
+  {
+    returnValue = 1;
+  }
+  else if(square[7] == square[5] && square[5] == square[3])
+  {
+    returnValue = 1;
+  }
+  else if(square[7] == square[8] && square[8] == square[9])
+  {
+    returnValue = 1;
+  }
+  else if(square[2] == square[5] && square[5] == square[8])
+  {
+    returnValue = 1;
+  }
+  else if(square[3] == square[6] && square[6] == square[9])
+  {
+    returnValue = 1;
+  }
+  else if(square[1] != '1' && square[2] != '2' && square[3] != '3' && 
+  square[4] != '4' && square[5] != '5' && square[6] != '6' && square[7] != '7'
+  && square[8] != '8' && square[9] != '9')
+  {
+    returnValue = 0;
+  }
+  else
+  {
+    returnValue = -1;
+  }
+}
+
+void displayBoard()
+{
+  
+}
+
+void markBoard(char mark)
+{
+  
+}
