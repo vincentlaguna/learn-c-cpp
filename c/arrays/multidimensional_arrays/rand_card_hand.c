787806b1347464f3ccde8b29cee5355e065ff973
vincentlaguna <vincentlaguna@live.com>
2020-06-05 03:55:52 +0000

    random hand of cards in C

--- c/arrays/multidimensional_arrays/rand_card_hand.c
+++ c/arrays/multidimensional_arrays/rand_card_hand.c
@@ -9,5 +9,28 @@
 int main(void)
 {
   bool in_hand[NUM_SUITS][NUM_RANKS] = {false};
+  int num_cards;
+  int rank;
+  int suit;
+  const char rank_code[] = {'2','3','4','5','6','7','8','9','t','j','k','a'};
+  const char suit_code[] = {'c','d','h','s'};
+  srand((unsigned) time(NULL));
+  
+  printf("Enter the number of cards in hand: ");
+  scanf("%d", &num_cards);
+  
+  printf("Your hand: ");
+  while(num_cards > 0)
+  {
+    suit = rand() % NUM_SUITS; // Picks a random suit
+    rank = rand() % NUM_RANKS; // Picks a random rank
+    if(!in_hand[suit][rank])
+    {
+      in_hand[suit][rank] = true;
+      num_cards--;
+      printf(" %c%c", rank_code[rank], suit_code[suit]);
+    }
+  }
+  printf("\n");
   return 0;  
 }
