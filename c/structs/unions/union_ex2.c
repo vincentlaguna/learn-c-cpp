adc2a33e52006ea20ac5e54d4e89506587c98e85
vincentlaguna <vincentlaguna@live.com>
2020-06-07 04:31:33 +0000

    structures - unions - C

---
+++ c/structs/unions/union_ex2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+union bookInfo
+{
+  int year;
+  char name[10];
+};
+
+struct book
+{
+  char author[20];
+  char publisher[20];
+  union bookInfo info;
+};
+
+int main(void)
+{
+  struct book book1;
+  printf("Enter name of author | publisher | year: ");
+  scanf("%s %s %d", book1.author, book1.publisher, &book1.info.year);
+  
+  printf("The information entered is \n");
+  printf("Author -> %s\n", book1.author);
+  printf("Publisher -> %s\n", book1.publisher);
+  printf("Year -> %d\n", book1.info.year);
+  
+  fflush(stdin);
+  printf("Enter name of book ->");
+  gets(book1.info.name); // watch why this fails
+  
+  printf("Name of the book is -> %s\n", book1.info.name);
+  printf("Year -> %d", book1.info.year);
+  getchar();
+  
+  return 0;
+}
