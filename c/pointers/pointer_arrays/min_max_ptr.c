9b41386a311bacd9af1be5b208f13d5bc09b98c0
vincentlaguna <vincentlaguna@live.com>
2020-06-10 21:45:07 +0000

    min-max pointer array exercise completed - C

--- c/pointers/pointer_arrays/min_max_ptr.c
+++ c/pointers/pointer_arrays/min_max_ptr.c
@@ -1,33 +1,41 @@
+// Finds the largest and smallest elements in an array
 #include <stdio.h>
 
 #define N 10
 
-int min_max(int *ptrA);
+void max_min(int a[], int n, int *max, int *min);
 
 int main(void)
 {
   int values[N];
-  int i;
+  int big;
+  int small;
   
   printf("Please enter %d numbers -> ", N);
-  for(i = 0; i < N; i++)
-    scanf("%d",&values[i]);
-  min_max(values);
+  for(int i = 0; i < N; i++)
+    scanf("%d", &values[i]);
+  
+  max_min(values, N, &big, &small);
+  
+  printf("Largest: [%d]\n", big);
+  printf("Smallest: [%d]\n", small);
+  
   return 0;
 }
 
-int min_max(int *ptrA)
+void max_min(int a[], int n, int *max, int *min)
 {
-  int min = 0;
-  int max = 0;
-  for(int i = 0; i < N; i++)
-    if(ptrA[i] > min)
-     {
-      max = ptrA[i];
-     }
-  else if(i < max)
+  *max = 0;
+  *min = a[0];
+  for(int i = 1; i < n; i++)
   {
-    min = ptrA[i];
+    if(a[i] > *max)
+    {
+      *max = a[i];
+    }
+    else if(a[i] < *min)
+    {
+      *min = a[i];
+    }
   }
-  printf("Min value: [%d], Max value: [%d]\n", min, max);
 }
