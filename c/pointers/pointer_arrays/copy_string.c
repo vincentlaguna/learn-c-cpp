800a17e6ae1c3adbe5fdf91a33e1995c2c8c108b
vincentlaguna <vincentlaguna@live.com>
2020-06-12 02:25:06 +0000

    character pointers exercise (copy strings 1, 2) - C

--- c/pointers/pointer_arrays/copy_string.c
+++ c/pointers/pointer_arrays/copy_string.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+void copyString(char *to, char *from);
+
 int main(void)
 {
+  char string1[] = "A string to be copied";
+  char string2[50];
+  copyString(string2, string1);
+  printf("Copied strings: %s -> %s\n", string1, string2);
   return 0;
 }
 
@@ -15,12 +21,24 @@ void copyString(char to[], char from[])
   to[i] = '\0';
 }
 */
-// Pointers used
+
+/* Pointers used
 void copyString(*to, *from)
 {
-  for(; *to != '\0'; ++from, ++to)
+  for(; *to != '\0'; ++from, ++to) // Pre-decrement, keep it from going "out of bounds"
   {
     *to = *from;
   }
   *to = '\0'
+}
+*/
+
+// Example optimized
+void copyString(char *to, char *from)
+{
+  while(*from) // NULL character == 0, will jump out then
+  {
+    *to++ = *from++;
+  }
+  *to = '\0';
 }
