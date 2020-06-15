2c834a660381cdd3df89ef3ca887f881371ab1fb
vincentlaguna <vincentlaguna@live.com>
2020-06-15 04:35:12 +0000

    structs containing pointers - working commit - C

--- c/structs/struct_pointers/structs_containing_pointers_ex1.c
+++ c/structs/struct_pointers/structs_containing_pointers_ex1.c
@@ -8,5 +8,14 @@ struct intPtrs
 
 int main(void)
 {
+  struct intPtrs pointers;
+  int i1 = 100;
+  int i2;
+  pointers.p1 = &i1;
+  pointers.p2 = &i2;
+  *pointers.p2 = -97;
+  
+  printf("i1 = [%i], *pointers.p1 = [%i]\n", i1, *pointers.p1);
+  printf("i1 = [%i], *pointers.p2 = [%i]\n", i2, *pointers.p2);
   return 0;
 }
