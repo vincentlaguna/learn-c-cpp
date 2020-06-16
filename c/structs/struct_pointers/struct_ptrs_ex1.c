2b7dba5d46e6a2495d65618224b64bc37334bf23
vincentlaguna <vincentlaguna@live.com>
2020-06-16 21:46:29 +0000

    structure pointer example update ...again - C

--- c/structs/struct_pointers/struct_ptrs_ex1.c
+++ c/structs/struct_pointers/struct_ptrs_ex1.c
@@ -3,6 +3,7 @@
 
 void readItem(struct item *i);
 void printItem(struct item *i);
+
 struct item
 {
   char *itemName;
@@ -18,19 +19,33 @@ int main(void)
   pItem = &itm;
   // (*pItem).amount =  // use -> instead
   pItem->itemName = (char*) malloc(30 * sizeof(char)); // Memory allocation for variable pointed to
-  if(pItem)
+  if(pItem == NULL)
     exit(-1);
+  readItem(pItem);
+  printItem(pItem);
   
+  free(pItem);
   return 0;
 }
 
 void readItem(struct item *i)
 {
   printf("Enter a product name -> ");
-  scanf(i->itemName);
+  scanf("%s", i->itemName);
+  
+  printf("Enter quantity -> \n");
+  scanf("%d", &i->qty);
+  
+  printf("Enter price -> \n");
+  scanf("%f", &i->price);
+  
+  i->amount = (float)i->qty * i->price;
 }
 
 void printItem(struct item *i)
 {
-  return 0;
+  printf("Name: [%s]\n", i->itemName);
+  printf("Price: [%f]\n", i->price);
+  printf("Quantity: [%d]\n", i->qty);
+  printf("Total amount: [%.2f]\n", i->amount);
 }
