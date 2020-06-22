bda2d4ce4977dff914d88fe249ed7d87765820bf
vincentlaguna <vincentlaguna@live.com>
2020-06-22 21:28:01 +0000

    read lines update (need working commit still) - C

--- c/file_ops/read_lines.c
+++ c/file_ops/read_lines.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+//#define FILENAME "../file_ops/modify_llorem.txt"
 
 int main(void)
-{
+{ 
+  FILE *fp = NULL;
+  char ch;
+  int linesCount = 0;
+  fp = fopen("../file_ops/modify_llorem.txt", "r");
+  
+  if(fp == NULL)
+  {
+    printf("File does not exist...\n");
+  }
+  while((ch = fgetc(fp)) != EOF)
+  {
+    if(ch == '\n')
+      linesCount++;
+  }
+  fclose(fp);
+  fp = NULL;
+  
+  printf("Total number of lines are -> [%d]\n", linesCount);
   return 0;
 }
