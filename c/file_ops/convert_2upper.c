cc6610384a7831321300f42fe55b7ef856796151
vincentlaguna <vincentlaguna@live.com>
2020-06-23 20:41:22 +0000

    update to convert to lower - C

--- c/file_ops/convert_2upper.c
+++ c/file_ops/convert_2upper.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
-#include <string.h>
+#include <stlib.h>
 
 int main(void)
 {
-  FILE *fp;
-  fp = fopen("../file_ops/modify_llorem.txt", "w+");
+  FILE *fp = NULL;
+  FILE *fp1 = NULL;
+  char ch = '';
+  
+  fp = fopen("../file_ops/modify_llorem.txt", "r");
   
   if(fp == NULL)
-  {
-    perror("Error opening the file");
     return(-1);
+
+  fp1 = fopen("temp.txt", "w");
+  
+  if(fp1 == NULL)
+    return(-1);
+    
+  while((ch = fgetc(fp)) != EOF )
+  {
+    if(isLower(ch))
+    {
+      ch = ch - 32; // ASCII lower-case
+    }
   }
   
   fclose(fp);
