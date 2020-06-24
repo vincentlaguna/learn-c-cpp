d8a38ff8b902ca0605ea892e31cf713b2b425e4e
vincentlaguna <vincentlaguna@live.com>
2020-06-24 21:49:39 +0000

    month reminder exercise - first commit - C

---
+++ c/personal_projects/month_reminder.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_REMIND 60 // Max number of reminders
+#define MAX_LEN 60 // Max length of reminder message
+
+int main(void)
+{
+  return 0;
+}
