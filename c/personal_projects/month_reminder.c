c9cb3918db72f6708fd8b29c9aa2c39e6aa65e99
vincentlaguna <vincentlaguna@live.com>
2020-06-24 22:01:52 +0000

    month remind update - C

--- c/personal_projects/month_reminder.c
+++ c/personal_projects/month_reminder.c
@@ -26,5 +26,15 @@ int main(void)
 
 int read_line(char str[], int n)
 {
+  int ch; i = 0;
   
+  while((ch = getchar()) != '\n')
+  {
+    if(i < n)
+    {
+      str[i++] = ch;
+    }
+  }
+  str[i] = '\0';
+  return i;
 }
