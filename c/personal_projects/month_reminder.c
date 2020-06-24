613525d0c70792605de74a48062a4463d81ebce5
vincentlaguna <vincentlaguna@live.com>
2020-06-24 21:55:34 +0000

    month remind exercise update - C

--- c/personal_projects/month_reminder.c
+++ c/personal_projects/month_reminder.c
@@ -2,9 +2,29 @@
 #include <string.h>
 
 #define MAX_REMIND 60 // Max number of reminders
-#define MAX_LEN 60 // Max length of reminder message
+#define MSG_LEN 60 // Max length of reminder message
+
+int read_line(char str[], int n);
 
 int main(void)
 {
+  char reminders[MAX_REMIND][MSG_LEN + 3];
+  char day_str[3], msg_str[MSG_LEN + 1];
+  int day, i, j, num_remind = 0;
+  
+  for(;;)
+  {
+    if(num_remind == MAX_REMIND)
+    {
+      printf("-- No Space Left --\n");
+      break;
+    }
+    
+  }
   return 0;
+}
+
+int read_line(char str[], int n)
+{
+  
 }
