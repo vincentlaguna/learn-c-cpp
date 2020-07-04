e7f3645422aee39d73a3f633a25df68b4e48aadc
vincentlaguna <vincentlaguna@live.com>
2020-07-04 20:26:06 +0000

    extern variables (Udemy Advanced C Programming course) commit #2

--- c/variables/extern_ex1.c
+++ c/variables/extern_ex1.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 
 int i = 5;
-
+char text[255][50];
 void foo(void);
+int count;
+/*void write_extern(void)
+{
+  printf("count is %d\n", count);
+}*/
+// Instead of defining the function, declare it as extern instead:
+extern void write_extern();
 
 int main(void)
 {
   printf("%i ", i);
   foo();
   printf("%i\n", i);
-  
-  return 0;
+  count = 4;
+  write_extern();
+  return 1;
 }
--- c/variables/extern_foo.c
+++ c/variables/extern_foo.c
@@ -1,6 +1,15 @@
+#include <stdio.h>
+
+extern char text[][50];
+extern int count;
 
 void foo(void)
 {
   extern int i;
   i = 100;
+}
+
+void write_extern(void)
+{
+  printf("count is %d\n", count);
 }
