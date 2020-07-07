37c50974c3afe2b3d5d4393e3034d4fa5637a5f4
vincentlaguna <vincentlaguna@live.com>
2020-07-07 21:50:30 +0000

    flexible array challenge commit #2 - C

--- c/arrays/flex_array.c
+++ c/arrays/flex_array.c
@@ -1,6 +1,28 @@
+// Flexible array challenge
 #include <stdio.h>
+#include <malloc.h>
+
+struct myArray
+{
+  int len;
+  int arr[];
+};
 
 int main(void)
 {
+  struct myArray *m = NULL;
+  int arraySize = 0;
+  
+  printf("Please enter the size of the flexible array -> \n");
+  scanf("%d", &arraySize);
+  
+  size_t size = sizeof(struct myArray);
+  m = malloc(size + (sizeof(int) * arraySize));
+  m->len = arraySize;
+  m->arr[0] = 55;
+  m->arr[1] = 199;
+  
+  printf("array is -> %d : %d\n", m->arr[0], m->arr[1]);
+  
   return 0;
 }
