3b30e6faa3080c21a3c8f8b6cb1fc86c7a81b9fc
vincentlaguna <vincentlaguna@live.com>
2020-07-07 04:32:05 +0000

    planets - pending

--- c/functions/cmd_line_args_planets.c
+++ c/functions/cmd_line_args_planets.c
@@ -11,18 +11,14 @@ int main(int argc, char *argv[])
   int i;
   int j;
   
-  for(i = 1; i < argc; i++)
-  {
+  for(i = 1; i < argc; i++) {
     for(j = 0; j < NUM_PLANETS; j++)
-    {
-      if(strcmp(argv[i], planets[j]) == 0)
-      {
+      if(strcmp(argv[i], planets[j]) == 0) {
         printf("%s is a planet %d\n", argv[i], j + 1);
         break;
-      }
-      if( j == NUM_PLANETS)
+        }
+      if(j == NUM_PLANETS)
         printf("%s is not a planet\n", argv[i]);
-    }
-  }
+    } 
   return 0;
 }
