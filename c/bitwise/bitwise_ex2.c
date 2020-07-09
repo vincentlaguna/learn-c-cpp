7877b6ea52e14248aeb0185768b86ec65467a426
vincentlaguna <vincentlaguna@live.com>
2020-07-09 22:19:49 +0000

    bitwise example 2 - udemy course - commit #1 - C

--- c/bitwise/bitwise_ex2.c
+++ c/bitwise/bitwise_ex2.c
@@ -4,6 +4,14 @@ int main(void)
 {
   short int w1 = 25;
   short int w2 = 77;
+  short int w3 = 0;
   
+  w3 = w1 & w2;
+  printf("%d\n", w3);
+  
+  //0000000000011001 // Binary 25
+  //0000000001001101 // Binary 77
+  //----------------
+  //0000000000001001 = Decimal 9
   return 0;
 }
