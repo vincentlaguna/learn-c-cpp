b74734957db870dedd94099dcb100ad0b490c58f
vincentlaguna <vincentlaguna@live.com>
2020-07-09 22:29:04 +0000

    bitwise example 2 - udemy course - commit #2 - C

--- c/bitwise/bitwise_ex2.c
+++ c/bitwise/bitwise_ex2.c
@@ -6,12 +6,29 @@ int main(void)
   short int w2 = 77;
   short int w3 = 0;
   
+  short int x1 = 147;
+  short int x2 = 61;
+  short int x3 = 0;
+  
   w3 = w1 & w2;
+  //w3 = w1 & 3; // Will set all the bits in w3 to 0, except the right-most 2 bits
   printf("%d\n", w3);
   
   //0000000000011001 // Binary 25
   //0000000001001101 // Binary 77
   //----------------
   //0000000000001001 = Decimal 9
+  
+  x3 = x1 | x2;
+  printf("%d\n", x3);
+  
+  //10010011 // Binary 147
+  //00111101 // Binary 61
+  //----------------
+  //10111111 = Decimal 191
+  
+  
+  
+  
   return 0;
 }
