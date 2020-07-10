5d4ab7b9b7466bcffa2ef29eda7eb1fba15c6058
vincentlaguna <vincentlaguna@live.com>
2020-07-10 20:13:43 +0000

    bitwise challenge 1 - commit #2 - C

--- c/bitwise/bitwise_challenge_1.c
+++ c/bitwise/bitwise_challenge_1.c
@@ -11,11 +11,11 @@ int main(void)
   
   int d_numLeftShiftResult = 0;
   
-  long long b_numComplimentResult = 0;
-  long long b_num2ComplimentResult = 0;
+  long long b_num1ComplementResult = 0;
+  long long b_num2ComplementResult = 0;
   long long b_num1ANDnum2Result = 0;
   long long b_num1ORnum2Result = 0;
-  long long b_num1EXORnum2REsult = 0;
+  long long b_num1ExORnum2Result = 0;
   long long b_num1LeftShiftResult = 0;
   
   printf("Enter an integer (number input): ");
@@ -23,6 +23,26 @@ int main(void)
   
   printf("Enter another integer (bit position): ");
   scanf("%d", &num2);
+  
+  b_num1 = convertDecimalToBinary(num1);
+  b_num2 = convertDecimalToBinary(num2);
+  
+  int d_num1LeftShiftResult = num1 << 2;
+  
+  b_num1ComplementResult = convertDecimalToBinary(~num1);
+  b_num2ComplementResult = convertDecimalToBinary(~num2);
+  b_num1ANDnum2Result = convertDecimalToBinary(num1 & num2);
+  b_num1ORnum2Result = convertDecimalToBinary(num1 | num2);
+  b_num1ExORnum2Result = convertDecimalToBinary(num1 ^ num2);
+  b_num1LeftShiftResult = convertDecimalToBinary(num1 << 2);
+    
+  printf("The result of applying the ~ operator on number %d (%lld) is: %lld \n", num1, b_num1, b_num1ComplementResult);
+  printf("The result of applying the ~ operator on number %d (%lld) is: %lld \n", num2, b_num2, b_num2ComplementResult);
+  printf("The result of applying the & operator on number %d (%lld) and number %d (%lld) is: %lld \n", num1, b_num1, num2, b_num2, b_num1ANDnum2Result);
+  printf("The result of applying the | operator on number %d (%lld) and number %d (%lld) is: %lld \n", num1, b_num1, num2, b_num2, b_num1ORnum2Result);
+  printf("The result of applying the ^ operator on number %d (%lld) and number %d (%lld) is: %lld \n", num1, b_num1, num2, b_num2, b_num1ExORnum2Result);
+  printf("The result of applying the left shift operator << on number %d (%lld) by 2 places is number %d (%lld)\n", num1, b_num1, d_num1LeftShiftResult, b_num1LeftShiftResult);
+  
   return 0;  
 }
 
