7e3a0cb80053d19aa98c7a047ab34c210876192b
vincentlaguna <vincentlaguna@live.com>
2020-07-11 18:18:13 +0000

    better than average kata - codewars - commit #1 - C

--- c/personal_projects/codewars/better_than_avg.c
+++ c/personal_projects/codewars/better_than_avg.c
@@ -13,3 +13,17 @@ int better_than_average(int class_points[], int class_size, int your_points)
     return 1;
   return 0;
 }
+
+// Tests --Need to relocate and install library (criterion)
+#include <criterion/criterion.h>
+
+int better_than_average(int *, int, int);
+
+Test(better_than_average, ex_tests) {
+  int cp1[] = {2, 3};
+  int cp2[] = {100, 40, 34, 57, 29, 72, 57, 88};
+  int cp3[] = {12, 23, 34, 45, 56, 67, 78, 89, 90};
+  cr_assert(better_than_average(cp1, 2, 5));
+  cr_assert(better_than_average(cp2, 8, 75));
+  cr_assert_not(better_than_average(cp3, 9, 9));
+}
