13d60446231655c89239bfae0c98384721fcd01b
vincentlaguna <vincentlaguna@live.com>
2020-07-12 05:08:21 +0000

    packed data structure for bit fields - commit #1 - C

--- c/bit_manipulation/packed_data.c
+++ c/bit_manipulation/packed_data.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-struct packet_data
+struct packed_struct
 {
+  int count;
+  char c;
   unsigned int :3;
   unsigned int f1:1;
   unsigned int f2:1;
@@ -12,5 +14,14 @@ struct packet_data
 
 int main(void)
 {
+  struct packed_struct packed_data;
+  
+  packed_data.type = 7;
+  //unsigned int bit = packed_data.type; // variable created to access the element
+  unsigned int i = packed_data.index / 5 + 1;
+  
+  if(!packed_data.f2)
+    printf("packed_data.f2 -> [%d]\n", packed_data.f2);
+  
   return 0;
 }
