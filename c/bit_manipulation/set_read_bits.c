938f181022cbe088f88ccd86133ff4a4e7288228
vincentlaguna <vincentlaguna@live.com>
2020-07-12 00:40:02 +0000

    set and read bit challenge - working commit - C

--- c/bit_manipulation/set_read_bits.c
+++ c/bit_manipulation/set_read_bits.c
@@ -19,5 +19,12 @@ int main(void)
   // Right-shift num, position times and perform bitwise AND with 1
   bitStatus = (num >> position) & 1;
   printf("The %d bit is set to %d\n", position, bitStatus);
+  // Left shift 1, n times and perform bitwise OR with num
+  newNum = (1 << position) | num;
+  printf("\nBit set successfully.\n\n");
+  
+  printf("Number before setting %d bit -> %d (in decimal)\n", position, num);
+  printf("Number after setting %d bit -> %d (in decimal)\n", position, newNum);
+  
   return 0;
 }
