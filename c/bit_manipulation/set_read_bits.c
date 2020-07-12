c863caec7ad8785a0bcc6b8e357642cb197abb7f
vincentlaguna <vincentlaguna@live.com>
2020-07-12 00:24:18 +0000

    set and read bit challenge - commit #0 - C

---
+++ c/bit_manipulation/set_read_bits.c
@@ -0,0 +1,13 @@
+// Requirements for challenge example:
+// Enter a number: 10
+// Enter nth bit to check and set (0-31): 2
+// The 2 bit is set to 0
+// Bit set successfully
+// Number before setting 2 bit: 10 (in decimal)
+// Number after setting 2 bit: 14 (in decimal)
+#include <stdio.h>
+
+int main(void)
+{
+  return 0;
+}
