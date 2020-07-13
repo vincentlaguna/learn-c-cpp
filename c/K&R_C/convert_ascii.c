214d5064f81e1ddf08ef86e8468d0c6e2d99a910
vincentlaguna <vincentlaguna@live.com>
2020-07-13 20:57:45 +0000

    f to c conversion using pre-processor defines - commit #0 - C

--- c/K&R_C/convert_ascii.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-  char c1, c2, c3, c4, c5, c6;
-  printf("Enter any 6 characters of your choice: ");
-  scanf("%c %c %c %c %c %c", &c1, &c2, &c3, &c4, &c5, &c6);
-  printf("ASCII codes: %u, %u, %u, %u, %u, %u\n", c1, c2, c3, c4, c5, c6);
-}
---
+++ c/functions/f_to_c.c
@@ -0,0 +1,10 @@
+// Convert a Fahrenheit temperature to Celsius (using pre-processor defines)
+#include <stdio.h>
+
+#define FREEZING_PT 32.0f
+#define SCALE_FACTOR (5.0f / 9.0f)
+
+int main(void)
+{
+  return 0;
+}
