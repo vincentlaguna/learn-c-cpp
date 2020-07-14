ce2df325d8c3d6829050feff292146b16edf91cc
vincentlaguna <vincentlaguna@live.com>
2020-07-14 21:42:20 +0000

    goto example - commit #2 - C

--- c/functions/goto_function_ex1.c
+++ c/functions/goto_function_ex1.c
@@ -10,7 +10,17 @@ int main(void)
   {
     printf("%d. Enter a number: ", i);
     scanf("%lf", &number);
+    
+    if(number < 0.0)
+      goto jump;
+    
+    sum += number;
   }
+  
+  jump:
+    average = sum / (i - 1);
+    printf("Sum = %.2f\n", sum);
+    printf("Average = %.2f\n", average);
   
   return 0;
 }
