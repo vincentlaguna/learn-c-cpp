fa5d2391023f72e66ee65ef488964f6f106724b8
vincentlaguna <vincentlaguna@live.com>
2020-07-14 21:38:28 +0000

    goto example - commit #1 - C

--- c/functions/goto_function_ex1.c
+++ c/functions/goto_function_ex1.c
@@ -2,5 +2,15 @@
 
 int main(void)
 {
+  const int maxInput = 5;
+  int i = 0;
+  double number, average, sum = 0.0;
+  
+  for(i = 1; i <= maxInput; ++i)
+  {
+    printf("%d. Enter a number: ", i);
+    scanf("%lf", &number);
+  }
+  
   return 0;
 }
