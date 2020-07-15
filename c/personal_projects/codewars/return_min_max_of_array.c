e66beab0a8666090bbffb19b8d201f72c89e9ea5
vincentlaguna <vincentlaguna@live.com>
2020-07-15 21:35:35 +0000

    codewars exercise returning min-max array update - C

--- c/personal_projects/codewars/return_min_max_of_array.c
+++ c/personal_projects/codewars/return_min_max_of_array.c
@@ -13,4 +13,10 @@ int min(int *array, int arrayLength)
 {
   int mn = array[0];
   
+  for(int i = 0; i <= arrayLength - 1; i++)
+  {
+    if(array[i] < mn)
+      mn = array[i];
+  }
+  return mn;
 }
