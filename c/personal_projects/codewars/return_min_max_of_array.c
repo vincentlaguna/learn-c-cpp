a11496eceae5e11cf49251c469ee580a53f00468
vincentlaguna <vincentlaguna@live.com>
2020-07-15 21:30:56 +0000

    codewars exercise returning min-max array - C

--- c/personal_projects/codewars/return_min_max_of_array.c
+++ c/personal_projects/codewars/return_min_max_of_array.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+int min(int *array, int arrayLength);
+int max(int *array, int arrayLength);
+
 int main(main)
 {
+  
   return 0;
+}
+
+int min(int *array, int arrayLength)
+{
+  int mn = array[0];
+  
 }
