7ba49f10e4ffe3a5f3e7938255d645670fa52265
vincentlaguna <vincentlaguna@live.com>
2020-07-18 19:28:35 +0000

    rework struct examples update 1 - C

--- c/structs/struct_examples/struct_ex1.c
+++ c/structs/struct_examples/struct_ex1.c
@@ -1,14 +1,35 @@
 #include <stdio.h>
 #include <stdint.h>
 
-int main(void)
+struct DataSet
 {
-    char data1 = 0x11;
-    int data2  = 0xFFFFEEEE;
-    char data3 = 0x22;
-    short data4 = 0xABCD;
+    char data1;
+    int data2;
+    char data3;
+    short data4;    
+};
+
+int main(void)
+{   
+    struct DataSet data;
+    
+    data.data1  = 0x11;
+    data.data2  = 0xFFFFEEEE;
+    data.data3  = 0x22;
+    data.data4  = 0xABCD;
+    
+    uint8_t *ptr;
+    ptr = (uint8_t*)&data; // struct DataSet*
+    uint32_t totalSize = sizeof(struct DataSet);
     
     printf("Memory Address          Content \n");
     printf("================================\n");
+    
+    for(uint32_t i = 0; i < totalSize; i++)
+    {
+        printf("%p %13X\n", ptr, *ptr);
+        ptr++;
+    }
+    
     return 0;
 }
