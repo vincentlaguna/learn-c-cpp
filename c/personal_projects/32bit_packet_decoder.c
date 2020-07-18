ef539a0b8a01ac370518fa14a55b95b4386533a9
vincentlaguna <vincentlaguna@live.com>
2020-07-18 21:00:26 +0000

    32bit packet decoder challenge - commit #2 - C

--- c/personal_projects/32bit_packet_decoder.c
+++ c/personal_projects/32bit_packet_decoder.c
@@ -13,16 +13,22 @@
 
 typedef struct
 {
-  crc;
-  status;
-  payload;
-  bat;
-  sensor;
-  longAddr;
-  addrMode;
+  uint8_t crc;
+  uint8_t status;
+  uint16_t payload;
+  uint8_t bat;
+  uint8_t sensor;
+  uint8_t longAddr;
+  uint8_t addrMode;
 }Packet_t;
 
 int main(void)
 {
+  uint32_t packetValue;
+  printf("Enter 32-bit Packet Value: ");
+  scanf("%u", &packetValue);
+  
+  Packet_t packet;
+  
   return 0;
 }
