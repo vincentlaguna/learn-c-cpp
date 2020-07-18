37613cd3a287fb4a267ee92222fb16b6e3b9bef7
vincentlaguna <vincentlaguna@live.com>
2020-07-18 20:53:12 +0000

    32bit packet decoder challenge - commit #1 - C

--- c/personal_projects/32bit_packet_decoder.c
+++ c/personal_projects/32bit_packet_decoder.c
@@ -1,5 +1,26 @@
+/*******************************************************************************
+*                                                                              *
+*                           32-bit Packet Decoder                              *
+* Format:                                                                      *
+*                                                                              *
+* <ADDR_MODE>|<SHORT_ADDR>|<LONG_ADDR>|<SENSOR>|<BAT>|<PAYLOAD>|<STATUS>|<CRC> *
+*      1            2           8         3       3       12       1       2   *
+*                                                                              *
+********************************************************************************/
+
 #include <stdio.h>
 #include <stdint.h>
+
+typedef struct
+{
+  crc;
+  status;
+  payload;
+  bat;
+  sensor;
+  longAddr;
+  addrMode;
+}Packet_t;
 
 int main(void)
 {
