d5c2a5136559087f96d295f230865c134b274ace
vincentlaguna <vincentlaguna@live.com>
2020-07-18 21:30:56 +0000

    32bit packet decoder challenge - commit #3 - C

--- c/personal_projects/32bit_packet_decoder.c
+++ c/personal_projects/32bit_packet_decoder.c
@@ -19,6 +19,7 @@ typedef struct
   uint8_t bat;
   uint8_t sensor;
   uint8_t longAddr;
+  uint8_t shortAddr;
   uint8_t addrMode;
 }Packet_t;
 
@@ -29,6 +30,24 @@ int main(void)
   scanf("%u", &packetValue);
   
   Packet_t packet;
+  
+  packet.crc        = (uint8_t)(packetValue & 0x3);
+  packet.status     = (uint8_t)((packetValue >> 2) & 0x1);
+  packet.payload    = (uint16_t)((packetValue >> 3) & 0xFFF);
+  packet.bat        = (uint8_t)((packetValue >> 15) & 0x7); 
+  packet.sensor     = (uint8_t)((packetValue >> 18) & 07);
+  packet.longAddr   = (uint8_t)((packetValue >> 21) & 0xFF);
+  packet.shortAddr  = (uint8_t)((packetValue >> 29) & 0x3);
+  packet.addrMode   = (uint8_t)((packetValue >> 31) & 0x3);
+  
+  printf("crc       : [%x] %u\n", packet.crc);
+  printf("status    : [%x]\n", packet.status);
+  printf("payload   : [%x]\n", packet.payload);
+  printf("bat       : [%x]\n", packet.bat);
+  printf("sensor    : [%x]\n", packet.sensor);
+  printf("longAddr  : [%x]\n", packet.longAddr);
+  printf("shortAddr : [%x]\n", packet.shortAddr);
+  printf("addrMode  : [%x]\n", packet.addrMode);
   
   return 0;
 }
