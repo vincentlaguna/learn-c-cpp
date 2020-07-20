2e13208e70b29cd41dc92fcb0e93364f90d56a33
vincentlaguna <vincentlaguna@live.com>
2020-07-20 02:42:51 +0000

    spaghetti code using goto and jump - commit #1 C

--- c/functions/spaghetti_jump.c
+++ c/functions/spaghetti_jump.c
@@ -7,6 +7,36 @@ int main(void)
   int j, k = 0;
   
   outerLoop:
-  
+   if(i < value)
+   {
+     j = i;
+     
+    innerLoop:
+      if(j < value)
+      {
+        printf(" ");
+        j++;
+        goto innerLoop;
+      }
+   }
+   else 
+   {
+     innerLoop2:
+      if(k != ((2 * i)))
+      {
+        if(k == 0 || k == (2 * i) - 3)
+        {
+          printf("*");
+        }
+        printf(" ");
+        k++;
+        goto innerLoop2;
+      }
+      k = 0;
+      printf("\n");
+      i++;
+      goto outerLoop;
+   }
+   
   return 0;
 }
