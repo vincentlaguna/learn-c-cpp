5904a112b9e69a4bb95276c76a0fe7dc2bf37fe8
vincentlaguna <vincentlaguna@live.com>
2020-07-24 16:27:26 +0000

    variable length array exercise 3 - commit #4 - C

--- c/arrays/vla_ex3.c
+++ c/arrays/vla_ex3.c
@@ -6,6 +6,8 @@ int main(void)
   uint8_t someData[10] = {0xff, 0xff, 0xff, 0xff, 0xff, 
                           0xff, 0xff, 0xff, 0xff, 0xff};
   
+  printf("0th element value -> %x\n", someData[0]);
+  // same as *(someData + 0) // deferenced point to 0th element
   printf("2nd data item = %x\n", *(someData +1)); // de-referenced value
   *(someData + 1) = 0x9; 
   // Same as someData[1] = 0x9 (pointing to second element in array)
