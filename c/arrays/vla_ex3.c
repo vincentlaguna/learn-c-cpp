d71cea5a0b7cf1daecda384dcd7029e49054fb0d
vincentlaguna <vincentlaguna@live.com>
2020-07-24 16:22:43 +0000

    variable length array exercise 3 - commit #3 - C

--- c/arrays/vla_ex3.c
+++ c/arrays/vla_ex3.c
@@ -5,8 +5,8 @@ int main(void)
 {
   uint8_t someData[10] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
   
-  printf("2nd data item = %x\n", *(someData +1));
-  *(someData + 1) = 0x9;
+  printf("2nd data item = %x\n", *(someData +1)); // Displaying de-referenced value
+  *(someData + 1) = 0x9; // Same as someData[1] = 0x9
   printf("After : 2nd data item = %x\n", *(someData+1));
   
   return 0;
