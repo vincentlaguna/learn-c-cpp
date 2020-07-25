32bf6f342271aee0fef53f5d33121287ae30cc73
vincentlaguna <vincentlaguna@live.com>
2020-07-25 23:07:02 +0000

    student record exercise - commit #2 - C

--- c/personal_projects/student_records.c
+++ c/personal_projects/student_records.c
@@ -10,11 +10,43 @@ typedef struct
   char  dob[15];
   int   semester;
 }STUDENT_INFO_t;
+// Max record this program can store
+int max_records = 10;
 // A global array of structure to hold the record of 10 students 
 STUDENT_INFO_t students[10];
-// Max record this program can store
+// Function protoypes
+void display_menu(void);
+int read_menu_code(void);
+void decode_menu_code(int8_t menu_code);
+void display_all_records(STUDENT_INFO_t *record, int8_t max_records);
+int check_roll_number(int roll_number, STUDENT_INFO_t *record, int8_t max_records);
 
 int main(void)
 {
   return 0;
+}
+
+void display_menu(void)
+{
+  
+}
+
+int read_menu_code(void)
+{
+  
+}
+
+void decode_menu_code(int8_t menu_code)
+{
+  
+}
+
+void display_all_records(STUDENT_INFO_t *record, int8_t max_records)
+{
+  
+}
+
+int check_roll_number(int roll_number, STUDENT_INFO_t *record, int8_t max_records)
+{
+  
 }
