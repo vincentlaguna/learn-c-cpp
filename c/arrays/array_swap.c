8f5a444de4a4ce72fe6e1dbeff6d5c2d0b187fa5
vincentlaguna <vincentlaguna@live.com>
2020-07-25 19:20:43 +0000

    array swap exercise - commit #3 - C

--- c/arrays/array_swap.c
+++ c/arrays/array_swap.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 
 void wait_for_user_input(void);
-void display_array(uint8_t *pArray, uint32_t nitem);
+void display_array(uint32_t *pArray1, uint32_t nitem);
 
 int main(void)
 {
@@ -24,21 +24,25 @@ int main(void)
   
   for(uint32_t i = 0; i < nItem1; i++)
   {
-    printf("Enter %d element of the array: ", i);
+    printf("Enter %d element of array 1: ", i);
     scanf("%d", &array1[i]);
   }
-  printf("Contents of the array before the swap ->\n");
-  display(array1, nItem1);
-  wait_for_user_input();
   
+  for(uint32_t i = 0; i < nItem2; i++)
+  {
+    printf("Enter %d element of array 2: ", i);
+    scanf("%d", &array2[i]);
+  }
+  printf("Contents of array before swap -> \n");
+  display_array(array1, nItem1)
   return 0;
 }
 
-void display_array(uint8_t *pArray, uint32_t nitem)
+void display_array(int32_t *pArray1, uint32_t nitem)
 {
   for(uint32_t i = 0; i < nitem; i++)
   {
-    printf("%d ", *(pArray + i));
+    printf("%d ", *(pArray1 + i));
     
   }
 }
