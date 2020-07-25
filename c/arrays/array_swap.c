b62c7c3815b187cd65bb349e60f6f95fdad67f56
vincentlaguna <vincentlaguna@live.com>
2020-07-25 06:07:41 +0000

    array swap exercise - commit #2 - C

--- c/arrays/array_swap.c
+++ c/arrays/array_swap.c
@@ -2,17 +2,45 @@
 #include <stdint.h>
 
 void wait_for_user_input(void);
+void display_array(uint8_t *pArray, uint32_t nitem);
 
 int main(void)
 {
   uint32_t nItem1; 
   uint32_t nItem2;
-  printf("[Array swapping program____________########]")
-  printf("Enter the elements of array-1 and array-2: ");
-  scanf("%d %d", &nItem1, &nItem2);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           )
+  printf("[Array swapping program____________########]\n");
+  printf("\nEnter the elements of array-1 and array-2: ");
+  scanf("%d %d", &nItem1, &nItem2);
+  
+  if((nItem1 < 0) || (nItem2 < 0))
+  {
+    printf("The number of elements cannot be negative.\n");
+    wait_for_user_input();
+    return 0;
+  }
+  
+  uint32_t array1[nItem1];
+  uint32_t array2[nItem2];
+  
+  for(uint32_t i = 0; i < nItem1; i++)
+  {
+    printf("Enter %d element of the array: ", i);
+    scanf("%d", &array1[i]);
+  }
+  printf("Contents of the array before the swap ->\n");
+  display(array1, nItem1);
   wait_for_user_input();
   
   return 0;
+}
+
+void display_array(uint8_t *pArray, uint32_t nitem)
+{
+  for(uint32_t i = 0; i < nitem; i++)
+  {
+    printf("%d ", *(pArray + i));
+    
+  }
 }
 
 void wait_for_user_input(void)
