63cdb4f045415c7c72fe62675b404d94af3ba0f7
vincentlaguna <vincentlaguna@live.com>
2020-07-25 22:49:24 +0000

    student record exercise - commit #1 - C

--- c/personal_projects/student_records.c
+++ c/personal_projects/student_records.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+// Definition of a student record
+typedef struct
+{
+  int   rollNumber;
+  char  name[100];
+  char  branch[50];
+  char  dob[15];
+  int   semester;
+}STUDENT_INFO_t;
+// A global array of structure to hold the record of 10 students 
+STUDENT_INFO_t students[10];
+// Max record this program can store
 
 int main(void)
 {
