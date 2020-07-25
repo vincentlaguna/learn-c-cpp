90b12ead70ac6d1eaaef9f579029f7ca9501e60c
vincentlaguna <vincentlaguna@live.com>
2020-07-25 20:38:01 +0000

    string constants and string literals - commit #1 - C

--- c/arrays/string_constant_ex1.c
+++ c/arrays/string_constant_ex1.c
@@ -2,5 +2,10 @@
 
 int main(void)
 {
+  char msg1[] = "Hello how are you?"; // Array variable (stored in RAM)
+  printf("Message is: %s\n", msg1);
+  printf("Address of 'msg1' variable is -> %p\n", &msg1);
+  printf("Value of 'msg1' variable -> %p\n", msg1);
+  
   return 0;
 }
