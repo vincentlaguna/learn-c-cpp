6bf9e1c792a84624f82057cc33fee1ce41562119
vincentlaguna <vincentlaguna@live.com>
2020-07-26 18:50:54 +0000

    pre-processor defined macro definition use - working commit #1 - C

--- c/macros/preprocessor_defines_ex1.c
+++ c/macros/preprocessor_defines_ex1.c
@@ -1,37 +1,33 @@
 #include <stdio.h>
-
-#define PI_VALUE (3.1415f)
 //#define AREA_OF_CIRCLE(r) PI_VALUE * r * r // -> Not safe!
+#define PI_VALUE (3.1415f)
 #define AREA_OF_CIRCLE(x) ((PI_VALUE)*(x)*(x)) // Safe
+#define AREA_CIRCLE
+#define AREA_TRIANGLE
+
 
 int main(void)
 {
   float area_circle; 
   area_circle = AREA_OF_CIRCLE(1+1);
-  printf("Area of circle -> %f\n", area_circle);
+  printf("(ex.) Area of circle -> %f\n", area_circle);
   
-  #if 1
-    
+  #ifdef AREA_CIRCLE
     float radius = 0;
     printf("\nYou have now entered the circle area calculation program\n");
     printf("Enter the radius: ");
     scanf("%f", &radius);
     area_circle = AREA_OF_CIRCLE(radius);
     printf("Area of circle -> %f\n", area_circle);
-    
-  #if 0
-    
-    return 0;
-  
   #endif
   
-  #endif
-  
+  #ifdef AREA_TRIANGLE
     float base, height;
     printf("\nYou have now entered the triangle area calculation program\n");
     printf("Enter base and height: ");
     scanf("%f%f", &base, &height);
     printf("Area of triangle -> %f\n", (0.5 * base * height));
-    
+  #endif
+  
   return 0;
 }
