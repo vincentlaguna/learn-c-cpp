848b956632b7a1b9e0b2f6806d8f98c99cf7c0ea
vincentlaguna <vincentlaguna@live.com>
2020-07-28 21:09:06 +0000

    advanced convert to upper exercise - commit #2 - C

--- c/file_ops/adv_convert2upper.c
+++ c/file_ops/adv_convert2upper.c
@@ -6,6 +6,10 @@ void convertCase(FILE *fptr, const char *path);
 
 int main(void)
 {
+  FILE *fPtr = NULL; // File pointer to hold reference of input file
+  char path[100];
+  
+  printf("")
   return 0;
 }
 
