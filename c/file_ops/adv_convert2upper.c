089776198c4a14ea7f4e4021497a0cd8fceaa762
vincentlaguna <vincentlaguna@live.com>
2020-07-28 21:07:02 +0000

    advanced convert to upper exercise - commit #1 - C

--- c/file_ops/adv_convert2upper.c
+++ c/file_ops/adv_convert2upper.c
@@ -2,7 +2,14 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+void convertCase(FILE *fptr, const char *path);
+
 int main(void)
 {
   return 0;
+}
+
+void convertCase(FILE *fptr, const char *path)
+{
+  
 }
