b69f9033d82b2a5d79158d46a9dd6c941d90056b
vincentlaguna <vincentlaguna@live.com>
2020-07-28 21:17:59 +0000

    advanced convert to upper exercise - commit #5 - C

--- c/file_ops/adv_convert2upper.c
+++ c/file_ops/adv_convert2upper.c
@@ -17,13 +17,17 @@ int main(void)
   if(fPtr == NULL) // fopen() return NULL if unable to open file in given mode
   {
     printf("\nUnable to open file!\n");
-    printf("Please check whether file exists and you have read privileges\n");
+    printf("Please check whether file exists and you have read privilege\n");
     exit(EXIT_FAILURE);
   }
+  
+  convertCase(fPtr, path);
+  
   return 0;
 }
 
 void convertCase(FILE *fptr, const char *path)
 {
-  
+  FILE *dest = NULL; // Create temp file
+  char ch = '\0'; // NULL character
 }
