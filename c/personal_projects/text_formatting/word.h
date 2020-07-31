2107ef30e493bc3c01e869a510859b4dc3c144f4
vincentlaguna <vincentlaguna@live.com>
2020-07-31 21:47:18 +0000

    text formatting exercise from C - Programming book - commit #1 - C

---
+++ c/personal_projects/text_formatting/word.h
@@ -0,0 +1,10 @@
+#ifndef WORD_H
+#define WORD_H
+
+/***********************************************************************
+*                                                                      *
+* read_word: Reads the next word from the input and stores it in word. *
+* Makes word empty if no word could be read because of EOF.            *
+* Truncates the word if it's length exceeds len.                       *
+*                                                                      *
+************************************************************************
