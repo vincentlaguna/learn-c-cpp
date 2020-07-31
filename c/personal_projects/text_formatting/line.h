90409ccee6037f51edb8d1f5ce3aa8dab94cef65
vincentlaguna <vincentlaguna@live.com>
2020-07-31 21:51:53 +0000

    text formatting exercise from C - Programming book - commit #4 - C

--- c/personal_projects/text_formatting/line.h
+++ c/personal_projects/text_formatting/line.h
@@ -1,6 +1,10 @@
 #ifndef LINE_H
 #define LINE_H
-// clear_line(void);
+
+// clear_line: Clears the current line.
+void clear_line(void);
+// add_word: Adds word to the end of the current line.
+// If this is not the first word on the line, it puts one space before word
 void clear_line(void);
 
 /* Contextual design without implementation:
