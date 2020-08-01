fac136f9dc52c36553895ab5de050824e89caa11
vincentlaguna <vincentlaguna@live.com>
2020-08-01 19:04:21 +0000

    text formatting exercise from C Programming book - commit #12 - C

--- c/personal_projects/text_formatting/line.c
+++ c/personal_projects/text_formatting/line.c
@@ -30,5 +30,33 @@ void add_word(const char *word)
 
 int space_remaining(void)
 {
+  return MAX_LINE_LEN - line_len;
+}
+
+void write_line(void)
+{
+  int extra_spaces, spaces_to_insert, i, j;
+  extra_spaces = MAX_LINE_LEN - line_len;
   
+  for(i = 0; i < line_len; i++)
+  {
+    if(line[i] != ' ')
+      putchar(line[i]);
+    else
+    {
+      spaces_to_insert = extra_spaces / (num_words - 1);
+      
+      for(j = 1; j <= spaces_to_insert + 1; j++)
+        putchar(' ');
+      extra_spaces -= spaces_to_insert;
+      num_words--;
+    }
+  }
+  putchar('\n');
+}
+
+void flush_line(void)
+{
+  if(line_len > 0)
+    puts(line);
 }
