0c2136e901eb9aa72cdebb512587275c5ef51226
vincentlaguna <vincentlaguna@live.com>
2020-08-01 20:02:36 +0000

    array sort exercise - linkedin learning - commit #2 - C

--- c/arrays/arraysort_1.c
+++ c/arrays/arraysort_1.c
@@ -8,6 +8,15 @@ int main(void)
 {
   int numbers[SIZE];
   int x, outer, inner, temp;
+  // populate the array
+  srand((unsigned)time(NULL));
   
+  for(x = 0; x < SIZE; x++)
+    numbers[x] = rand() % 100 + 1;
+  // display the unsorted array
+  for(x = 0; x < SIZE; x++)
+    printf(" %3d", numbers[x]);
+  printf("\n");
+
   return 0;
 }
