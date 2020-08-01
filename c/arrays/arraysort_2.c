ea9a725039317208625c77666a20d68121792a5f
vincentlaguna <vincentlaguna@live.com>
2020-08-01 20:47:02 +0000

    array sort exercise 2 - linkedin learning - working commit - C

--- c/arrays/arraysort_2.c
+++ c/arrays/arraysort_2.c
@@ -2,7 +2,38 @@
 
 #define SIZE 10
 
+void displayArray(int *a);
+
 int main(void)
 {
+  int numbers[SIZE] = { 55, 44, 99, 11, 77, 22, 33, 88, 66, 0};
+  int outer, inner, temp;
+  // Display the unsorted array
+  puts("Unsorted array ->");
+  displayArray(numbers);
+  // Sort the array
+  for(outer = 0; outer < SIZE; outer++)
+    for(inner = outer + 1; inner < SIZE; inner++)
+    {
+      if(numbers[outer] > numbers[inner])
+      {
+        temp = numbers[outer];
+        numbers[outer] = numbers[inner];
+        numbers[inner] = temp;
+        displayArray(numbers);
+      }
+    }
+  // Display sorted array
+  puts("\nSorted Array -> \n");
+  displayArray(numbers);
   return 0;
+}
+
+void displayArray(int *a)
+{
+  int x;
+  
+  for(x = 0; x < SIZE; x++)
+    printf(" %3d", a[x]);
+  putchar('\n');
 }
