ebbcb545f6ec5972642cd17f24795751f1afbdc3
vincentlaguna <vincentlaguna@live.com>
2020-08-01 04:40:47 +0000

    text formatting exercise from C - Programming book - commit #9 - C

--- c/personal_projects/text_formatting/word.c
+++ c/personal_projects/text_formatting/word.c
@@ -16,7 +16,12 @@ void read_word(char *word, int len)
   int pos = 0;
   
   while((ch = read_char()) == ' ')
+    ;
+  while(ch != ' ' && ch != EOF)
   {
-    
+    if(pos < len)
+      word[pos++] = ch;
+    ch = read_char();
   }
+  word[pos] = '\0';
 }
