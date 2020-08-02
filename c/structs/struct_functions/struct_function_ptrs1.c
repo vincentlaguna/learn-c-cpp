3e69c0033a1aa457faca9ebc042fa38c3cd6039b
vincentlaguna <vincentlaguna@live.com>
2020-08-02 18:49:51 +0000

    struct function exercise modified for use of pointers - advanced C programming - linkedin learning - commit #0 - C

---
+++ c/structs/struct_functions/struct_function_ptrs1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct person
+{
+  char name[32];
+  int age;
+  float iq;
+};
+
+struct person *allocateStruct(void);
+struct person fetchStruct(struct person *p);
+void showStruct(struct person *p);
+
+int main(void)
+{
+  
+  return(0);
+}
+
+struct person *allocateStruct(void)
+{
+  
+}
+
+struct person fetchStruct(struct person *p)
+{
+  
+}
+
+void showStruct(struct person *p)
+{
+  
+}
