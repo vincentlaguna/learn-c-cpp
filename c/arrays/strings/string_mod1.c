3a03ac1a1900865e4e9b93f0fe4f0b3a5e6ce8b6
vincentlaguna <vincentlaguna@live.com>
2020-08-02 02:32:26 +0000

    modify string exercise 1 - linkedin learning - commit #1 - C

--- c/arrays/strings/string_mod1.c
+++ c/arrays/strings/string_mod1.c
@@ -5,6 +5,14 @@ void modify(char *s);
 
 int main(void)
 {
+  char string[64];
+  
+  printf("\nType some text: ");
+  fgets(string, 64, stdin);
+  modify(string);
+  puts("\nHere is your modified string -> \n");
+  printf("%s\n", string);
+  
   return 0;
 }
 
