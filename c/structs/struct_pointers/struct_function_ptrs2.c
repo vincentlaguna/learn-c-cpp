d27dd3a78cefc960b5d034e8584642903c1bf347
vincentlaguna <vincentlaguna@live.com>
2020-08-02 19:12:00 +0000

    struct function challenge with use of pointers - advanced C programming - linkedin learning - commit #0 - C

---
+++ c/structs/struct_pointers/struct_function_ptrs2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct person
+{
+  char name[32];
+  int age;
+};
+
+struct person *allocateStructure(void);
+void fillStructure(struct person *p);
+void showStructure(struct person *p);
+
+int main(void)
+{
+  return(0);
+}
+
+struct person *allocateStructure(void)
+{
+  
+}
+
+void fillStructure(struct person *p)
+{
+  
+}
+
+void showStructure(struct person *p)
+{
+  
+}
