6bca53103608bb61df7dc767bbf75d9720f9f722
vincentlaguna <vincentlaguna@live.com>
2020-08-02 19:22:12 +0000

    struct function challenge with use of pointers - advanced C programming - linkedin learning - working commit - C

--- c/structs/struct_pointers/struct_function_ptrs2.c
+++ c/structs/struct_pointers/struct_function_ptrs2.c
@@ -13,20 +13,39 @@ void showStructure(struct person *p);
 
 int main(void)
 {
+  
+  struct person *you;
+  you = allocateStructure();
+  fillStructure(you);
+  showStructure(you);
+  
   return(0);
 }
 
 struct person *allocateStructure(void)
 {
+  struct person *p;
+  
+  p = (struct person *)malloc(sizeof(struct person));
+  if(p == NULL)
+  {
+    perror("Unable to allocate structure");
+    exit(1);
+  }
+  return(p);
   
 }
 
 void fillStructure(struct person *p)
 {
-  
+  printf("Type your first name: ");
+  scanf("%s", p->name); // !&p? = p is already a string array of characters
+  printf("Enter your age: ");
+  scanf("%d", &p->age);
 }
 
 void showStructure(struct person *p)
 {
-  
+  printf("Your name is %s, and you are %d years old\n", p->name, p->age);
+  //printf("%s has an IQ of %.1f\n", p->name, p->iq);
 }
