4f4e3141038c00594cf31b2ae7857607888cc258
vincentlaguna <vincentlaguna@live.com>
2020-08-02 18:30:02 +0000

    struct function exercise 1 - advanced C programming - linkedin learning - commit #0 - C

---
+++ c/structs/struct_functions/struct_function1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+struct person
+{
+  char name[32];
+  int age;
+  float iq;
+};
+
+struct person author;
+
+void showStruct(struct person p);
+
+int main(void)
+{
+  return(0);
+}
+
+void showStruct(struct person p)
+{
+  
+}
