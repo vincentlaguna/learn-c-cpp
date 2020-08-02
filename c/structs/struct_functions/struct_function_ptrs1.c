5a7cd297e16c941f2098feded14bb0c9fb41265e
vincentlaguna <vincentlaguna@live.com>
2020-08-02 19:01:37 +0000

    struct function exercise modified for use of pointers - advanced C programming - linkedin learning - working commit - C

--- c/structs/struct_functions/struct_function_ptrs1.c
+++ c/structs/struct_functions/struct_function_ptrs1.c
@@ -10,26 +10,41 @@ struct person
 };
 
 struct person *allocateStruct(void);
-struct person fetchStruct(struct person *p);
+void fetchStruct(struct person *p);
 void showStruct(struct person *p);
 
 int main(void)
 {
+  struct person *author;
+  
+  author = allocateStruct();
+  fetchStruct(author);
+  showStruct(author);
   
   return(0);
 }
 
 struct person *allocateStruct(void)
 {
-  
+  struct person *p;
+  p = (struct person *)malloc(sizeof(struct person));
+  if(p == NULL)
+  {
+    perror("Unable to allocate structure");
+    exit(1);
+  }
+  return(p);
 }
 
-struct person fetchStruct(struct person *p)
+void fetchStruct(struct person *p)
 {
-  
+  strcpy(p->name, "Dan Gookin");
+  p->age = 54;
+  p->iq = 287.5;
 }
 
 void showStruct(struct person *p)
 {
-  
+  printf("Author %s is %d years old\n", p->name, p->age);
+  printf("%s has an IQ of %.1f\n", p->name, p->iq);
 }
