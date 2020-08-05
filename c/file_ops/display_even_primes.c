07c244666f5564ff7c1e166020ece1abc78179db
vincentlaguna <vincentlaguna@live.com>
2020-08-05 00:55:36 +0000

    display even and primes from text file - Udemy Advanced C Programming Course - commit #1 - C

--- c/file_ops/display_even_primes.c
+++ c/file_ops/display_even_primes.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 // Function devlarations
-
+int isEven(const int);
+int isPrime(const int);
 int main(void)
 {
   return(0);
+}
+
+int isEven(const int)
+{
+  
+}
+
+int isPrime(const int)
+{
+  
 }
