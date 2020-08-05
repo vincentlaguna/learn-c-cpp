a5624ca5126a6559c0e93c7e1dc2dc6acc96856d
vincentlaguna <vincentlaguna@live.com>
2020-08-05 21:40:19 +0000

    display even and primes from text file - Udemy Advanced C Programming Course - commit #3 - C

--- c/file_ops/display_even_primes.c
+++ c/file_ops/display_even_primes.c
@@ -9,6 +9,14 @@ int main(void)
   FILE *fPtrIn = NULL; // Initialize file pointer
   int num = 0;
   int success = 0;
+  fPtrIn = fopen("numbers.txt", "r"); // Open the input file for reading
+  // fopen() return NULL if unable to open file in given mode
+  if(fPtrIn == NULL) // Unable to open the file, hence exit
+  { 
+    printf("Unable to open the file!\n");
+    printf("Please check whether the file exists and you have read/write permissions!\n");
+    exit(EXIT_FAILURE);
+  }
   
   return(1);
 }
