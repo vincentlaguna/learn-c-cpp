bb0822d81409d84b80e0525b081af51e5423ab19
vincentlaguna <vincentlaguna@live.com>
2020-08-07 05:09:33 +0000

    recursion example 1 - Udemy Advanced C Programming Course - working commit - C

--- c/functions/recursion_ex1.c
+++ c/functions/recursion_ex1.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+int recursion_function(int n);
+
 int main(void)
 {
+  printf("Recursion Function ->\n");
+  printf("= %d\n", recursion_function(10));
   return(0);
+}
+
+int recursion_function(int n)
+{
+  if(n == 0)
+    return(1);
+    printf("%d * ", n);
+  return (n * recursion_function(n-1)); 
 }
