ae8b26d2e82dcd6c772e4ee6ea6df63b74e6cb94
vincentlaguna <vincentlaguna@live.com>
2020-08-07 22:39:22 +0000

    pointer memory allocation 1 - Linkedin Master C Language Pointers course - working commit - C

--- c/pointers/dynamic_memory_allocation/adv_ptr_malloc1.c
+++ c/pointers/dynamic_memory_allocation/adv_ptr_malloc1.c
@@ -3,5 +3,38 @@
 
 int main(void)
 {
+  int *storage;
+  int x;
+  // Use malloc to set the buffer size to store 5 integers
+  storage = (int *)malloc(sizeof(int) * 5);
+  
+  if(storage == NULL)
+  {
+    fprintf(stderr, "Memory Allocation Failed!\n");
+    exit(1);
+  }
+  puts("Memory Allocated");
+  // Initialize the output values
+  for(x = 0; x < 5; x++)
+  {
+    *(storage + x) = (x + 1) * 11;
+    printf("%d\n", *(storage + x));
+  }
+  // Now increase the buffer size by two integer values
+  storage = (int *)realloc(storage, sizeof(int) * 7);
+  
+  if(storage == NULL)
+  {
+    fprintf(stderr, "Reallocation Failed!\n");
+    exit(1);
+  }
+  puts("Buffer re-sized");
+  // Add the final two values
+  *(storage + 5) = 66;
+  *(storage + 6) = 77;
+  // Output the results
+  for(x = 0; x < 7; x++)
+    printf("%d\n", *(storage + 1));
+    
   return(0);
 }
