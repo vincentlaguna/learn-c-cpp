ca062a06085b87116948585a15846112ff3c74fe
vincentlaguna <vincentlaguna@live.com>
2020-08-07 17:12:26 +0000

    recusrion challenges - Udemy Advanced Concepts in C - working commit #2 - C

--- c/functions/recursion_challenge1.c
+++ c/functions/recursion_challenge1.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 int sumOfRange(int n1);
+int findGCD(int num1, int num2);
 
 int main(void)
 {
   int n1 = 0;
   int sum = 0;
+  
   
   printf("\n\nRecursion: Calculate the sum of numbers from 1 to n: \n");
   printf("----------------------------------------------------\n");
@@ -24,4 +26,9 @@ int sumOfRange(int n1)
   result = n1 + sumOfRange(n1 - 1);
   printf(" + %d", n1);
   return result;
+}
+
+int findGCD(int num1, int num2)
+{
+  
 }
