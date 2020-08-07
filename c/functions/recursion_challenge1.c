efb0d206660f2d4ce8c8a1f4e4c4de94a2976c29
vincentlaguna <vincentlaguna@live.com>
2020-08-07 16:58:15 +0000

    recusrion challenge 1 - Udemy Advanced Concepts in C - commit #1 - C

--- c/functions/recursion_challenge1.c
+++ c/functions/recursion_challenge1.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 
-int sumOfRange(int)
+int sumOfRange(void);
 
 int main(void)
 {
+  int n1 = 0;
+  int sum = 0;
+  
+  printf("\n\nRecursion: Calculate the sum of numbers from 1 to n: \n");
+  printf("----------------------------------------------------\n");
+  printf("\nInput the last number of the range starting from 1: ");
+  scanf("%d", &n1);
+  
   return(0);
+}
+
+int sumOfRange(void)
+{
+  
 }
