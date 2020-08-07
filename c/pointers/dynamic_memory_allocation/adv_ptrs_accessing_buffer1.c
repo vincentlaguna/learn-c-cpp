6d810e7245b4301edef84230b2563396546f0c76
vincentlaguna <vincentlaguna@live.com>
2020-08-07 23:04:00 +0000

    pointer memory allocation 1 - accessing buffer using pointers - Linkedin Master C Language Pointers course - working commit - C

--- c/pointers/dynamic_memory_allocation/adv_ptrs_accessing_buffer1.c
+++ c/pointers/dynamic_memory_allocation/adv_ptrs_accessing_buffer1.c
@@ -3,5 +3,38 @@
 
 int main(void)
 {
+  int *base;
+  int *p;
+  int x;
+  // Allocate storage
+  base = (int *)malloc(sizeof(int) * 10);
+  
+  if(base == NULL)
+  {
+    fprintf(stderr, "Allocation Failure!\n");
+    exit(1);
+  }
+  // Retain *base
+  p = base;
+  // Fill storage
+  for(x = 0; x < 10; x++)
+  {
+    *p = x * 100;
+    // Reference the next integer
+    p++;
+  }
+  puts("Memory allocated and filled.");
+  // Output the buffer's contents
+  p = base;
+  
+  for(x = 0; x < 10; x++)
+  {
+    printf("%d\n", *p);
+    p++;
+  }
+  free(base);
+  base = NULL;
+  puts("Memory freed! Goodbye!");
+  
   return(0);
 }
