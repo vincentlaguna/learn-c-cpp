2ad5b488f89db6958eaa5bd1fef0fda763cc4400
vincentlaguna <vincentlaguna@live.com>
2020-08-07 16:18:23 +0000

    factorial recursion refactored to exit gracefully when handling too large of an integer input - C

--- c/functions/recursion_ex1.c
+++ c/functions/recursion_ex1.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int recursion_function(int n);
+long long recursion_function(long long n);
 
 int main(void)
 {
-  printf("Recursion Function ->\n");
-  printf("= %d\n", recursion_function(10));
+  long long input;
+  printf("\nRecursion Function ->\n");
+  printf("\nPlease enter integer for factorial: ");
+  scanf("%lld", &input);
+  if(input > 25)
+  {
+    printf("Integer value too big for factorial!\n");
+    exit(1);
+  }
+  printf("\nOutput ->  ");
+  printf(" = %lld\n", recursion_function(input));
+  putchar('\n');
   return(0);
 }
 
-int recursion_function(int n)
+long long recursion_function(long long n)
 {
   if(n == 0)
     return(1);
-    printf("%d * ", n);
+    printf("%lld * ", n);
   return (n * recursion_function(n-1)); 
 }
