fbfbaec87440fae717f2a4484dc21ae07a55cb0c
vincentlaguna <vincentlaguna@live.com>
2020-08-07 23:17:52 +0000

    pointer memory allocation 1 - using malloc in structures - Linkedin Master C Language Pointers course - working commit - C

--- c/structs/struct_pointers/adv_ptrs_struct_malloc1.c
+++ c/structs/struct_pointers/adv_ptrs_struct_malloc1.c
@@ -3,5 +3,28 @@
 
 int main(void)
 {
+  struct person
+  {
+    char *name;
+    int *age;
+  }my;
+  
+  char n[] = "Vincent";
+  // Allocate memory for the age integer
+  my.age = (int *)malloc(sizeof(int) * 1);
+  
+  if(my.age == NULL)
+  {
+    fprintf(stderr, "Allocation error\n");
+    exit(1);
+  }
+  // Assign value
+  *my.age = 34;
+  // Because 'n' is an array and 'my.name' is a pointer,
+  // you can just copy the base address again
+  my.name = n;
+  // Output results
+  printf("%s is %d years old.\n", my.name, *my.age);
+  
   return(0);
 }
