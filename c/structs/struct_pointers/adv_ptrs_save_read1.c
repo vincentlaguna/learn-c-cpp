8ba7a5ad6f86ad2eb2c3f9f64b3e85d0a8f7776a
vincentlaguna <vincentlaguna@live.com>
2020-08-08 01:50:09 +0000

    pointer memory allocation - using malloc in structures - save and read record challenge - Linkedin Master C Language Pointers course - commit #1 - C

--- c/structs/struct_pointers/adv_ptrs_save_read1.c
+++ c/structs/struct_pointers/adv_ptrs_save_read1.c
@@ -4,5 +4,56 @@
 
 int main(void)
 {
+  struct human
+  {
+    char name[32];
+    char profession[64];
+    int age;
+  } *toWrite;
+  
+  const char filename[] = "saved.dat";
+  char *r1;
+  char *r2;
+  FILE *fp;
+  // Allocate structure
+  toWrite = (struct human *)malloc(sizeof(struct human) * 1);
+  
+  if(toWrite == NULL)
+  {
+    fprintf(stderr, "Unable to allocate structure memory!\n");
+    exit(1);
+  }
+  // Fill the toWrite structure; prompt the user
+  printf("Enter your name: ");
+  r1 = fgets(toWrite->name, 32, stdin);
+  // Check for input errors
+  if(r1 == NULL)
+  {
+    fprintf(stderr, "Input Error.\n");
+    exit(1);
+  }
+  printf("What is your profession? ");
+  r2 = fgets(toWrite->profession, 64, stdin);
+  
+  if(r2 == NULL)
+  {
+    fprintf(stderr, "Unable to allocate structure memory!\n");
+    exit(1);
+  }
+  printf("How are old are you? ");
+  scanf("%d", &toWrite->age);
+  // Write the structure to a file
+  fp = fopen(filename, "w");
+  
+  if(fp == NULL)
+  {
+    fprintf(stderr, "Unable to allocate structure memory!\n");
+    exit(1);
+  }
+  // Write the structure
+  fwrite(toWrite, sizeof(struct human), 1, fp);
+  // Close the file
+  fclose(fp);
+  printf("Data written to %s\n", filename);
   return(0);
 }
