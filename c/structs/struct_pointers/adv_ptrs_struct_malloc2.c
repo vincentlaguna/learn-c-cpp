95b143aea7237bcb82e2a51f7d63d242511a6729
vincentlaguna <vincentlaguna@live.com>
2020-08-08 01:25:24 +0000

    pointer memory allocation 2 - using malloc in structures - Linkedin Master C Language Pointers course - working commit - C

--- c/structs/struct_pointers/adv_ptrs_struct_malloc2.c
+++ c/structs/struct_pointers/adv_ptrs_struct_malloc2.c
@@ -1,7 +1,42 @@
 #include <stdio.h>
-#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(void)
 {
+  struct person
+  {
+    char *name;
+    char *profession;
+    int *age;
+  } *first;
+  // Allocate the structure's memory
+  first = (struct person *)malloc(sizeof(struct person) * 1);
+  
+  if(first == NULL)
+  {
+    fprintf(stderr, "Unable to allocate storage.\n");
+    exit(1);
+  }
+  // Allocate structure members
+  first->name = (char *)malloc(sizeof(char) * 32);
+  first->profession = (char *)malloc(sizeof(char) * 32);
+  first->age = (int *)malloc(sizeof(int) * 2);
+  
+  if(first->name == NULL || first->profession == NULL || first->age == NULL)
+  {
+    fprintf(stderr, "Unable to allocate storage.\n");
+    exit(1);
+  }
+  // Assign structure members
+  strcpy(first->name, "Vincent Laguna");
+  printf("\nWhat is %s's profession? ", first->name);
+  scanf("%s", first->profession);
+  printf("How old is %s? ", first->name);
+  scanf("%d", first->age);
+  // Output results
+  printf("\n%s is %d years old, and works as a %s\n",
+          first->name, *first->age, first->profession);
+          
   return(0);
 }
