ada44f3ad59b3cf17b8c7dd0e33349c6d3ca4cca
vincentlaguna <vincentlaguna@live.com>
2020-08-08 20:56:03 +0000

    linked list exercise 3 - Linkedin Master C Pointers course - commit #1 - C

--- c/structs/linked_lists/linked_list_ex2.c
+++ c/structs/linked_lists/linked_list_ex2.c
@@ -39,4 +39,15 @@ char *input(void)
   }
   return(buffer);
 }
-
+// Allocate new structure
+struct item *allocate(void)
+{
+  struct item *p;
+  p = (struct item *)malloc(sizeof(struct item) * 1);
+  // Verify memory allocation
+  if(p == NULL)
+  {
+    fprintf(stderr, "Unable to allocate memory.\n");
+    exit(1);
+  }
+}
