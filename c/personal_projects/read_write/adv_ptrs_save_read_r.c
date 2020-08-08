d9f9e4ee59a8535d941c23c2e19de1520a7f07a3
vincentlaguna <vincentlaguna@live.com>
2020-08-08 02:12:57 +0000

    pointer memory allocation - using malloc in structures - save and read record challenge - Linkedin Master C Language Pointers course - working commit - C

--- c/personal_projects/read_write/adv_ptrs_save_read_r.c
+++ c/personal_projects/read_write/adv_ptrs_save_read_r.c
@@ -8,10 +8,33 @@ int main(void)
     char name[32];
     char profession[64];
     int age;
-  } *toWrite;
+  } *toRead;
   
   const char filename[] = "saved.dat";
-  char *r1;
-  char *r2;
   FILE *fp;
+  // Allocate structure
+  toRead = (struct human *)malloc(sizeof(struct human) * 1);
+  
+  if(toRead == NULL)
+  {
+    fprintf(stderr, "Unable to allocate structure memory.\n");
+    exit(1);
+  }
+  // Read the structure into memory
+  // Open the file
+  fp = fopen(filename, "r");
+  // Check for valid file
+  if(toRead == NULL)
+  {
+    fprintf(stderr, "Unable to read from %s.\n", filename);
+    exit(1);
+  }
+  // Read in the structure
+  fread(toRead, sizeof(struct human), 1, fp);
+  // Close the file
+  fclose(fp);
+  
+  exit(0);
+}
+  
   
