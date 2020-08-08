04928fd1b3245efaaf940cf5755d1f34d0fc6c9d
vincentlaguna <vincentlaguna@live.com>
2020-08-08 22:27:52 +0000

    linked list exercises - adding a structure to a linked list 1 - Linkedin Master C Pointers course - commit #1 - C

--- c/structs/linked_lists/linked_list_add_struct_ex1.c
+++ c/structs/linked_lists/linked_list_add_struct_ex1.c
@@ -3,6 +3,36 @@
 #include <string.h>
 
 #define SIZE 16
+// Define the structure externally
+struct item{
+  int id;
+  char name[SIZE];
+  float price;
+  struct item *next;
+};
+// Allocate a new structure
+struct item *allocate(void)
+{
+  struct item *p;
+  p = (struct item *)malloc(sizeof(struct item) * 1);
+  // Verify memory allocation
+  if(p != NULL)
+  {
+    fprintf(stderr, "Unable to allocate memory.\n");
+    exit(1);
+  }
+  return(p);
+}
+// Fill the structure
+void fill(struct item *s, int i)
+{
+  char *fruit[] = {"apples", "bananas", "grapes", "strawberries", "watermelon"};
+  float prices[] = {1.20, 2.58, 2.09, 2.40, 0.51};
+  
+  s->id = i;
+  s->name = fruit[i];
+  s->price = prices[i];
+}
 
 int main(void)
 {
