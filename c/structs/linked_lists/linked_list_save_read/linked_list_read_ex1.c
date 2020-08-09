6f77a42c3d4b9044dda66b8c9a9cc57e85dea67d
vincentlaguna <vincentlaguna@live.com>
2020-08-09 20:26:48 +0000

    linked list exercises - saving and readin a linked list to and from a file 1 - Linkedin Master C Pointers Course - commit #4 - C

--- c/structs/linked_lists/linked_list_save_read/linked_list_read_ex1.c
+++ c/structs/linked_lists/linked_list_save_read/linked_list_read_ex1.c
@@ -13,11 +13,36 @@ struct item
 // Allocate new structure
 struct item *allocate(void)
 {
-  
+  struct item *p;
+  p = (struct item *)malloc(sizeof(struct item) * 1);
+  // Verify memory allocation
+  if(p == NULL)
+  {
+    fprintf(stderr, "Unable to allocate memory.\n");
+    exit(1);
+  }
+  return(p);
+}
+// Output the list
+void output(struct item *s)
+{
+  while(s != NULL)
+  {
+    printf("%d: %s for $%.2f/pound\n",
+            s->id + 1,
+            s->name,
+            s->price);
+    s = s->next;
+  }
 }
 
 int main(void)
 {
-  puts("TEST");
+  struct item *first, *current;
+  int x;
+  FILE *fp;
+  // Open the file
+  
+  
   return(0);
 }
