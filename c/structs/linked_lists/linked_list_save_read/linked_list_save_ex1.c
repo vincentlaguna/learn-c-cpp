1e27d3988bb88ffe983a4e230c7cfa249018cd2f
vincentlaguna <vincentlaguna@live.com>
2020-08-09 20:03:13 +0000

    linked list exercises - saving a linked list 1 - Linkedin Master C Pointers Course - initial commit - C

---
+++ c/structs/linked_lists/linked_list_save_read/linked_list_save_ex1.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SIZE 16
+// Define structure externally
+struct item
+{
+  int id;
+  char name[SIZE];
+  float price;
+  struct item *next;
+};
+// Allocate a new structure
+struct item *allocate(void)
+{
+  struct item *p;
+  p = (struct item *)malloc(sizeof(struct item) * 1)
+  // Validate memory allocation
+  if(p == NULL)
+  {
+    fprintf(stderr, "Unable to allocate memory.\n");
+    exit(1);
+  }
+  return(p);
+}
+// Fill the structure
+void fill(struct item *s int i)
+char *fruit[] = {"apples", "bananas", "grapes", "strawberries", "watermelon"};
+  float prices[] = {1.20, 2.58, 2.09, 2.40, 0.51};
+  
+  s->id = i;
+  strcpy(s->name, fruit[i]);
+  s->price = prices[i];
+}
+// Output the list
+void output(struct item *s)
+{
+  while(s != NULL)
+  {
+    printf("%d: %s for $%.2f/pound\n",
+            s->id + 1,
+            s->name,
+            s->price);
+    s = s->next;
+  }
+}
+
+int main(void)
+{
+  struct item *first, *current;
+  int x;
+  FILE *fp;
+  
+  for(x = 0; x < 5; x++)
+  {
+    if(x == 0)
+    {
+      first = allocate();
+      current = first;
+    }
+    else
+    {
+      current->next = allocate();
+      current = current->next;
+    }
+    fill(current, x);
+  }
+  current->next = NULL;
+  output(first);
+  // Save the data
+  printf("Saving the data...");
+  fp = fopen("lldata.dat", "w");
+  // Verify file creation
+  if(fp == NULL)
+  {
+    fprintf(stderr, "Error creating file.\n");
+    exit(1);
+  }
+  // Write the 5 structures
+  fwrite(first, sizeof(struct item), 5, fp);
+  // Close the file
+  fclose(fp);
+  puts("Successfully saved!\n");
+  
+  return(0);
+}
