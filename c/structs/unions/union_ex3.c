d79d6ac8d83cb0fa97aae9edab7a1ad42acdaf95
vincentlaguna <vincentlaguna@live.com>
2020-08-11 22:33:46 +0000

    some practice with unions - commit #2 - C

--- c/structs/unions/union_ex3.c
+++ c/structs/unions/union_ex3.c
@@ -9,5 +9,12 @@ union student
 
 int main(void)
 {
+  union student record1;
+  union student record2;
+  // Assigning values to record1 variable
+  record1.letterGrade = 'B';
+  record1.roundGrade = 87;
+  record1.exactGrade = 86.50;
+  
   return(0);
 }
