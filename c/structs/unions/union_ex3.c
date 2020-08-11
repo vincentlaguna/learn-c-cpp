0e2d72bf9c96e735eb29ba43901791478fac231f
vincentlaguna <vincentlaguna@live.com>
2020-08-11 22:43:41 +0000

    some practice with unions - this is an example of how you can only access and assign one member ata time inside a union - working commit - C

--- c/structs/unions/union_ex3.c
+++ c/structs/unions/union_ex3.c
@@ -16,9 +16,18 @@ int main(void)
   record1.roundGrade = 87;
   record1.exactGrade = 86.50;
   // Display values
-  printf("Union record1 values example\n");
+  printf("\nUnion record1 values example\n\n");
   printf("Letter grade -> %c\n\n", record1.letterGrade);
   printf("Rounded grade -> %d\n\n", record1.roundGrade);
   printf("Exact grade -> %.2f\n\n", record1.exactGrade);
+  // Assigning values to record2 variable
+  printf("Union record2 values example\n\n");
+  record2.letterGrade = 'A';
+  printf("Letter grade -> %c\n\n", record2.letterGrade);
+  record2.roundGrade = 100;
+  printf("Rounded grade -> %d\n\n", record2.roundGrade);
+  record2.exactGrade = 99.50;
+  printf("Exact grade -> %.2f\n\n", record2.exactGrade);
+  
   return(0);
 }
