155fa278105e4e1fa868a8bb52a9685df31cd69a
vincentlaguna <vincentlaguna@live.com>
2020-08-11 04:42:40 +0000

    variadic functions - practice - C

--- c/functions/variadic_functions/var_func_ex1.c
+++ c/functions/variadic_functions/var_func_ex1.c
@@ -5,10 +5,19 @@ void func(int, ...);
 
 int main(void)
 {
+  func(2, 3, 5, 7, 10, 13);
   return(0);
 }
 
 void func(int n, ...)
 {
-  
+ int number, i = 0;
+ va_list start;
+ va_start(start, n);
+ while (i != 4)
+ {
+   number = va_arg(start, int);
+   i++;
+ }
+ printf("%d\n", number);
 }
