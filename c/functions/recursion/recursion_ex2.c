9867dc7db0a4894c2d2948107be61924525ea997
vincentlaguna <vincentlaguna@live.com>
2020-08-11 04:55:00 +0000

    recursion practice - commit #1 - C

--- c/functions/recursion/recursion_ex2.c
+++ c/functions/recursion/recursion_ex2.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
+int f1(int x);
+
 int main(void)
 {
+  int n;
+  n = f1(5);
+  
+  printf("%d\n", n);
+  
   return(0);
+}
+
+int f1(int x)
+{
+  int b;
+  
+  if(x == 1)
+    return 1;
+  else
+    b = x * f1(x - 1);
+    
+    return b;
 }
