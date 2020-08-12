29b2e03d0ad3058a5420c0bc2c7c9703dc98099e
vincentlaguna <vincentlaguna@live.com>
2020-08-12 22:02:12 +0000

    parts database project - C Programming Book - commit #1 - C

--- c/personal_projects/parts_database/inventory.c
+++ c/personal_projects/parts_database/inventory.c
@@ -2,3 +2,14 @@
 #include <stdio.h>
 #include "readline.h"
 
+#define NAME_LEN 25
+#define MAX_PARTS 100
+
+stuct part
+{
+  int number;
+  char name[NAME_LEN + 1];
+  int on_hand;
+} inventory[MAX_PARTS];
+// Number of parts currently stored
+int num_parts = 0;
