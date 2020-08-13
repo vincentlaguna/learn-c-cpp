42b1d97ff3c4ed879abd9bccecad3a6f31b9a604
vincentlaguna <vincentlaguna@live.com>
2020-08-13 21:59:42 +0000

    parts database project - C Programming Book - commit #6 - C

--- c/personal_projects/parts_database/inventory.c
+++ c/personal_projects/parts_database/inventory.c
@@ -27,4 +27,35 @@ void print(void);
  *       illegal code.                                                      *
  * **************************************************************************/
  
- 
+int main(void)
+{
+  char code;
+  for(;;)
+  {
+    printf("Enter operation code: ");
+    scanf("%c", &code);
+    // Skips to the end of line
+    while(getchar() != '\n')
+      ;
+    switch(code)
+    {
+      case 'i':
+        inser();
+        break;
+      case 's':
+        search();
+        break;
+      case 'u':
+        update();
+        break;
+      case 'p':
+        print();
+        break;
+      case 'q':
+        return(0);
+      default:
+        printf("Illegal code\n");
+    }
+    printf("\n");
+  }
+}
