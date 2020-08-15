122caa560e8b69b583408fb691c2c79ab4cabb1d
vincentlaguna <vincentlaguna@live.com>
2020-08-15 23:00:35 +0000

    sorting pointer arrays exercise 1 - working commit - C

--- c/pointers/pointers_to_pointers/sort_ptrs_ex1.c
+++ c/pointers/pointers_to_pointers/sort_ptrs_ex1.c
@@ -5,10 +5,10 @@
 int main(void)
 {
   char *weekdays[7] = {"Sunday", "Monday", "Tuesday", "Wednesday",
-		                "Thursday", "Friday", "Saturday"};
+		                   "Thursday", "Friday", "Saturday"};
   char *temp;
   int x, y;
-  puts("Unsorted:\n");
+  puts("\nUnsorted:\n");
   // Unsorted array
   for(x = 0; x < 7; x++)
   {
@@ -27,6 +27,12 @@ int main(void)
         weekdays[y] = temp;
       }
     }
+  }
+  // Sorted array
+  puts("\nSorted array:\n");
+  for(x = 0; x < 7; x++)
+  {
+    printf("%s\n", weekdays[x]);
   }
   putchar('\n');
   return(0);
