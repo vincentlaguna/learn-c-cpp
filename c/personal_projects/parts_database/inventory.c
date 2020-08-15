711d8c74057d0553757a56ef1a9a0061ba70ba51
vincentlaguna <vincentlaguna@live.com>
2020-08-15 16:05:38 +0000

    parts database project - C Programming Book - commit #16 - C

--- c/personal_projects/parts_database/inventory.c
+++ c/personal_projects/parts_database/inventory.c
@@ -145,3 +145,24 @@ void search(void)
  *         quantity, updates the database.                   *
  *************************************************************/
 
+void update(void)
+{
+  int i;
+  int number;
+  int change;
+  
+  printf("Enter part number: ");
+  scanf("%d", &number);
+  i = find_part(number);
+  
+  if(i >= 0)
+  {
+    printf("Enter change in quantity on hand: ");
+    scanf("%d", &change);
+    inventory[i].on_hand += change;
+  }
+  else
+  {
+    printf("Part not found.\n");
+  }
+}
