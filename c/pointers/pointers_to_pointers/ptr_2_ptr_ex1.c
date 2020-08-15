9488be07af6d99d76affbce88ca7c0076a2667ff
vincentlaguna <vincentlaguna@live.com>
2020-08-15 19:14:53 +0000

    pointers to pointers - fill exercise - Master C Language Pointers Linkedin course - commit #1 - C

--- c/pointers/pointers_to_pointers/ptr_2_ptr_ex1.c
+++ c/pointers/pointers_to_pointers/ptr_2_ptr_ex1.c
@@ -5,10 +5,23 @@ void fill(int **n);
 
 int main(void)
 {
+  int *numbers;
+  int x;
   return(0);
 }
 
 void fill(int **n)
 {
-  
+  int x;
+  *n = (int *)malloc(sizeof(int) * 4);
+  if(*n == NULL)
+  {
+    fprintf(stderr, "Unable to allocate memory.\n");
+    exit(1);
+  }
+  for(x = 0; x < 4; x++)
+  {
+    *(*n + x) = *(x + 1) * 11;
+  }
+  putchar('\n');
 }
