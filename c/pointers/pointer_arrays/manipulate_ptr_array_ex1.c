0db17223dd1e82aaeb66b6db0775bf441334b576
vincentlaguna <vincentlaguna@live.com>
2020-08-16 18:54:29 +0000

    manipulating a pointer array challenge - LinkedIn Master C Language Pointers - commit #1 - C

--- c/pointers/pointer_arrays/manipulate_ptr_array_ex1.c
+++ c/pointers/pointer_arrays/manipulate_ptr_array_ex1.c
@@ -3,8 +3,16 @@
 
 void get_input(char **b);
 
+// Create the array of pointers and output the results
 int main(void)
 {
+  char *names[5];
+  int x;
+  
+  for(x = 0; x < 5; x++)
+  {
+    
+  }
   return(0);
 }
 
