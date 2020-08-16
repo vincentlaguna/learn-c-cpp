8de2e8d5e24bbb9aa36f7268328770c53f5458c6
vincentlaguna <vincentlaguna@live.com>
2020-08-16 17:42:52 +0000

    memcpy() and memmove() string functions in C exercise - LinkedIn Master C Language Pointers - working commit #1 - C

--- c/pointers/memory_allocation/memcpy_memmove_ex1.c
+++ c/pointers/memory_allocation/memcpy_memmove_ex1.c
@@ -4,5 +4,15 @@
 
 int main(void)
 {
+  int a[] = {100, 101, 102, 103};
+  int b[4];
+  int x;
+  
+  memcpy(b, a, sizeof(int) * 4);
+  
+  for(x = 0; x < 4; x++)
+  {
+    printf("%d. -> %d = %d\n",x, a[x], b[x]);
+  }
   return(0);
 }
