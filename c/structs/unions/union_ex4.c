0f9fdf333211c421b4532f9292b6747689cd4eaa
vincentlaguna <vincentlaguna@live.com>
2020-08-18 21:38:19 +0000

    union example 4 - commit #2 - C

--- c/structs/unions/union_ex4.c
+++ c/structs/unions/union_ex4.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
-
+#define TITLE_LEN 40
+#define AUTHOR_LEN 20
+#define DESIGN_LEN 10
+/* // Struct implementation
 struct catalog_item
 {
   int stock_number;
   double price;
   int item_type;
-  
-}
+  char title[TITLE_LEN + 1];
+  char author[AUTHOR_LEN + 1];
+  int num_pages;
+  char design[DESIGN_LEN + 1];
+  int colors;
+  int sizes;
+};
+*/ // Union inside a structure implementation
+
+
 int main(void)
 {
   puts("Struct TEST");
