cedfb41f2653f4ce3b10da9ba2844a3e1c63ac2c
vincentlaguna <vincentlaguna@live.com>
2020-08-18 21:44:07 +0000

    union example 4 - commit #3 - C

--- c/structs/unions/union_ex4.c
+++ c/structs/unions/union_ex4.c
@@ -16,7 +16,31 @@ struct catalog_item
   int sizes;
 };
 */ // Union inside a structure implementation
-
+struct catalog_item
+{
+  int stock_number;
+  double price;
+  int item_type;
+  union
+  {
+    struct
+    {
+      char title[TITLE_LEN + 1];
+      char author[AUTHOR_LEN + 1];
+      int num_pages;
+    } book;
+    struct
+    {
+      char design[DESIGN_LEN + 1];
+    } mug;
+    struct
+    {
+      char design[DESIGN_LEN + 1];
+      int colors;
+      int sizes;
+    } shirt;
+  } item;
+};
 
 int main(void)
 {
