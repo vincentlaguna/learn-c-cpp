22062c15ebd81378a4b5414e176d5f6cc541e5e7
vincentlaguna <vincentlaguna@live.com>
2020-08-20 22:13:51 +0000

    monthly reminder version 2 - using pointers - dynamic string version - C

---
+++ c/personal_projects/monthly_reminder_list_v2.c
@@ -0,0 +1,19 @@
+// Prints a one-month reminder list (dynamic string version)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_REMIND 50
+#define MSG_LEN 60
+
+int read_line(char str[], int n);
+
+int main(void)
+{
+  return(0);
+}
+
+int read_line(char str[], int n)
+{
+  
+}
