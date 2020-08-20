2f926af0c8c5dc2a2285633f8cffd9a3909dce8a
vincentlaguna <vincentlaguna@live.com>
2020-08-20 22:17:13 +0000

    monthly reminder version 2 - using pointers - dynamic string version - commit #2 - C

--- c/personal_projects/monthly_reminder_list_v2.c
+++ c/personal_projects/monthly_reminder_list_v2.c
@@ -13,6 +13,17 @@ int main(void)
   char *reminders[MAX_REMIND];
   char day_str[3];
   char msg_str[MSG_LEN + 1];
+  int day, i, j, num_remind = 0;
+  
+  for(;;)
+  {
+    if(num_remind == MAX_REMIND)
+    {
+      printf("-- No Space Left! --\n");
+      break;
+    }
+  }
+  
   return(0);
 }
 
