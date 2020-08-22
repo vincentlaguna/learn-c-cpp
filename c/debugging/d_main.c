8d818728990032fc8dffc94fff1243fd2b2c0f02
vincentlaguna <vincentlaguna@live.com>
2020-08-22 20:09:14 +0000

    debugging exercise 1 - Advanced C Programming Course (Udemy) - commit #3 - C

--- c/debugging/d_main.c
+++ c/debugging/d_main.c
@@ -18,4 +18,35 @@ int sum(int x, int y, int z)
   #ifdef DEBUG
     fprintf(stderr, "*a = %d\n", *a);
   #endif
+  
+  return (c + x + y + z + *a) / 3;
+}
+
+int main(int argc, char *argv[])
+{
+  int i, j, k;
+  int result;
+  
+  #ifdef DEBUG
+    fprintf(stderr, "Number of parameters = %d\n", argc);
+  #endif
+  
+  if(argc == 1)
+  {
+    printf("Please specify three numbers as parameters.\n");
+    exit(1);
+  }
+  
+  i = atoi(argv[1]);
+  
+  #ifdef DEBUG
+    fprintf(stderr, "i = %d\n", i);
+  #endif
+  
+  j = atoi(argv[2]);
+  
+  #ifdef DEBUG
+    fprintf(stderr, "j = %d\n", j);
+  #endif
+  
 }
