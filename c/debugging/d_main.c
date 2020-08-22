d674af20cbdc9730df9c81980455118c2420cbab
vincentlaguna <vincentlaguna@live.com>
2020-08-22 19:59:04 +0000

    debugging exercise 1 - Advanced C Programming Course (Udemy) - initial commit #0 - C

---
+++ c/debugging/d_main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int sum(int x, int y, int z)
+{
+  char c = 2;
+  int *a = NULL;
+  
+  #ifdef DEBUG
+    fprintf(stderr, " = %d\n", );
+    fprintf(stderr, " = %d\n", );
+    fprintf(stderr, " = %d\n", );
+    fprintf(stderr, " = %d\n", );
+}
