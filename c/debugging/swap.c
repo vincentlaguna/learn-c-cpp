a096798e62bb472c7c622e1be48ac856b36d8dcd
vincentlaguna <vincentlaguna@live.com>
2020-08-23 22:28:05 +0000

    testing conditional compilation - working commit #4 - C

--- c/debugging/swap.c
+++ c/debugging/swap.c
@@ -7,15 +7,15 @@ int main(void)
   int y = 5;
   int tmp;
   
-  puts("Testing SWAP program -->-->-->");
-  printf("Current values: %d, %d\n", x, y);
+  puts("\nTesting SWAP program -->-->-->");
+  printf("\nCurrent values: %d, %d\n", x, y);
   #ifdef SWAP
     tmp = x;
     x   = y;
     y   = tmp;
   #endif
   
-  printf("After swap: %d, %d\n", x, y);
+  printf("After swap: %d, %d\n\n", x, y);
   
   return(0);
 }
