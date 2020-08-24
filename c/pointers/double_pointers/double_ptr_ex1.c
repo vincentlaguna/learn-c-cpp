2d85fe9f1434da19c175cc020407835669f65c70
vincentlaguna <vincentlaguna@live.com>
2020-08-24 22:01:39 +0000

    working with double pointers - example 1 - Advanced C Programming Course (Udemy) - working commit #6 - C

--- c/pointers/double_pointers/double_ptr_ex1.c
+++ c/pointers/double_pointers/double_ptr_ex1.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
+/*
 void foo(int **ptr) // Making use of double pointer = **ptr instead of *ptr
+{
+  int a = 5;
+  //ptr = &a; // This is just a LOCAL copy, it will not work
+  //*ptr = a; // This works, but it is better to make use of a double pointer
+  *ptr = &a;
+}
+
+int main(void)
+{
+  int *ptr = NULL;
+  ptr = (int *) malloc(sizeof(int));
+  *ptr = 10;
+  
+  printf("De-referernced pointer value -> %d\n", *ptr);
+  
+  foo(&ptr); // Passing in a double pointer instead of a single one (ptr)
+  printf("De-Referenced pointer value after foo() function -> %d\n", *ptr);
+  
+  return(0);
+}
+*/
+// Second example with allocating memory inside the function
+void foo(char **ptr) // Making use of double pointer = **ptr instead of *ptr
 {
   int a = 5;
   //ptr = &a; // This is just a LOCAL copy, it will not work
