c09790030460b8932c043bc0c01ac750ed54b269
vincentlaguna <vincentlaguna@live.com>
2020-08-26 22:14:04 +0000

    Function pointers challenge 1 - Advanced C Programming Course (Udemy) - commit #4 - C

--- c/pointers/function_pointers/function_ptr_challenge.c
+++ c/pointers/function_pointers/function_ptr_challenge.c
@@ -10,7 +10,11 @@ int sub(int a, int b) { return a - b; }
 int mlt(int a, int b) { return a * b; }
 int div(int a, int b) { return a / b; }
 // Array of pointers to functions (Task #1)
-int (*fpArray[]) (int, int) = {&add, &sub, &mlt, &div};
+int (*fpArray[]) (int, int) = {&add, &sub, &mlt, &div}; // Note* it must match function type and return parameter
+// Perform the given operation on the elements of the array (which are individual functions)
+int *performOp(int *a, int *b, int size, int (*f)(int, int)) // <-Adding func ptr params (Task #2)
+// Displays the elements of the array
+void display(int *x, int size);
 
 int main(void)
 {
