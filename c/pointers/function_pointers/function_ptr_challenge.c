7ce6b82a8efca02489b90577c0b4d5213244c25c
vincentlaguna <vincentlaguna@live.com>
2020-08-27 05:12:46 +0000

    Function pointers challenge 1 - Advanced C Programming Course (Udemy) - working commit #10 - C

--- c/pointers/function_pointers/function_ptr_challenge.c
+++ c/pointers/function_pointers/function_ptr_challenge.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <malloc.h>
 #include <string.h>
 // Some random numbers...
 int array1[] = {10,20,30,40,50,60,70,80,90,100};
@@ -12,7 +12,7 @@ int div(int a, int b) { return a / b; }
 // Array of pointers to functions (Task #1)
 int (*fpArray[]) (int, int) = {&add, &sub, &mlt, &div}; // Note* it must match function type and return parameter
 // Perform the given operation on the elements of the array (which are individual functions)
-int *performOp(int *a, int *b, int size, int (*f)(int, int)) // <-Adding func ptr params (Task #2)
+int *performOp(int *a, int *b, int size, int (*f)(int, int)); // <-Adding func ptr params (Task #2)
 // Displays the elements of the array
 void display(int *x, int size);
 // Main Method
@@ -25,7 +25,7 @@ int main(void)
   size = sizeof(array1); // Set size to minimum size of the two arrays
   
   if(sizeof(array2) < size)
-    size = sizeof(array2)
+    size = sizeof(array2);
   size = sizeof(int);
   
   while(choice != 5)
@@ -41,7 +41,7 @@ int main(void)
     
     if(choice == 5)
       break;
-    if(choice < 1 || > 5)
+    if(choice < 1 || choice > 5)
       continue;
     
     int j = choice - 1;
@@ -57,7 +57,7 @@ int main(void)
   return(0);
 }
 // Function definition of *performOp (Task #4)
-int *performOp(int *a, int *b, int size, (*f)(int, int))
+int *performOp(int *a, int *b, int size, int (*f)(int, int))
 {
   int *c = malloc(sizeof(int) * size);
   
