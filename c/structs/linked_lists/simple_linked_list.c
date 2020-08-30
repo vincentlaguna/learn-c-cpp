ad6ef3f058bbe8d874fc87389fa9c6cabd0f3f36
vincentlaguna <vincentlaguna@live.com>
2020-08-30 00:14:33 +0000

    simple linked-list example - commit #2 - C

--- c/structs/linked_lists/simple_linked_list.c
+++ c/structs/linked_lists/simple_linked_list.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 struct node *add_to_list(struct node *list, int n);
+struct node *read_numbers(vodi);
 
 int main(void)
 {
@@ -22,6 +23,9 @@ int main(void)
   new_node->next = first;
   first = new_node;
   
+  first = add_to_list(first, 10);
+  first = add_to_list(first, 20);
+  
   puts("Simple linked-list example TEST");
   
   return(0);
@@ -30,10 +34,29 @@ int main(void)
 struct node *add_to_list(struct node *list, int n)
 {
   struct node *new_node;
-  new_node = (struct node)malloc(sizeof(struct node));
+  new_node = (struct node *)malloc(sizeof(struct node));
   if(new_node == NULL)
   {
     printf("ERROR: malloc failed in FUNCTION -> add_to_list()\n");
     exit(EXIT_FAILURE);
+  }
+  new_node->value = n;
+  new_node->next = list;
+  return new_node;
+}
+
+struct node *read_numbers(void)
+{
+  struct node *first = NULL;
+  int n;
+  printf("Please enter a series of integers (0 to terminate): ");
+  
+  for(;;)
+  {
+    scanf("%d", &n);
+  
+    if(n == 0)
+      return first;
+    first = add_to_list(first, n);
   }
 }
