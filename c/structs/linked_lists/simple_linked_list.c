792f0576c84dc9e83d9e6c01f0b71154e9f26f0e
vincentlaguna <vincentlaguna@live.com>
2020-08-30 00:20:41 +0000

    simple linked-list example - commit #3 - C

--- c/structs/linked_lists/simple_linked_list.c
+++ c/structs/linked_lists/simple_linked_list.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct node
+{
+  int value; // Data stored in the node
+  struct node *next; //Pointer to the next node
+};
+struct node *new_node;
+
 struct node *add_to_list(struct node *list, int n);
-struct node *read_numbers(vodi);
+struct node *read_numbers(void);
 
 int main(void)
 {
+  /*
   struct node
   {
     int value; // Data stored in the node
@@ -13,20 +21,22 @@ int main(void)
   };
   struct node *first = NULL; // To later point to the first node in the list
   struct node *new_node; // Variable that points to the node temporarily, until inserted 
-  new_node = (struct node *)malloc(sizeof(struct node)); // Allocation of memory for new node
+  new_node = malloc(sizeof(struct node)); // Allocation of memory for new node
   new_node->value = 10;
   new_node->next = first;
   first = new_node;
   // Repeat for next node with different value
-  new_node = (struct node *)malloc(sizeof(struct node)); // Allocation of memory for new node
+  new_node = malloc(sizeof(struct node)); // Allocation of memory for new node
   new_node->value = 20;
   new_node->next = first;
   first = new_node;
-  
-  first = add_to_list(first, 10);
-  first = add_to_list(first, 20);
-  
+  */
+  struct node *first = NULL;
+  first = add_to_list(first, 100);
+  first = add_to_list(first, 200);
+
   puts("Simple linked-list example TEST");
+  read_numbers();
   
   return(0);
 }
@@ -34,7 +44,7 @@ int main(void)
 struct node *add_to_list(struct node *list, int n)
 {
   struct node *new_node;
-  new_node = (struct node *)malloc(sizeof(struct node));
+  new_node = malloc(sizeof(struct node));
   if(new_node == NULL)
   {
     printf("ERROR: malloc failed in FUNCTION -> add_to_list()\n");
