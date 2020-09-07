b183ae74b57d73b049d7f807f8b9181b9740260d
vincentlaguna <vincentlaguna@live.com>
2020-09-07 21:16:40 +0000

    time library use challenge 1 - commit #2 - C

--- c/personal_projects/time_functions/time_challenge1.c
+++ c/personal_projects/time_functions/time_challenge1.c
@@ -3,6 +3,24 @@
 
 int main(void)
 {
+  
+  /*
+  0.Use srand() with seed
+  1.Generate 50 random number (-0.5 to 0.5)
+  2.First line of output: number of data
+  3.Next 50 lines should be the 50 random numbers
+  4.Sort an array of doubles from lowest to highest
+  5.Create a function that takes a double array and a size parameter
+  which generates some random double values
+  6.void fillArray(double ar[], int n);
+  7.Create a function that shows the array (takes a double array and size)
+  8.void showArray(const double ar[], int n);
+  9.Main should contain: create array, fill it with random numbers,
+  display it, sort it using qsort, display the sorted array (0-15 doubles)
+  10.Write a program that displays the current time
+  11.Write a program to compute the number of seconds passed since
+  the beginning of the current month
+  */
   puts("Time challenge 1 TEST");
   return(0);
 }
