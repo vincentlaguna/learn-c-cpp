6551ffdf865f4aaeac60953fd7686d8cfb5c61b4
vincentlaguna <vincentlaguna@live.com>
2020-09-07 23:29:05 +0000

    time library use challenge 2 - commit #3 - C

--- c/personal_projects/time_functions/time_challenge1.c
+++ c/personal_projects/time_functions/time_challenge1.c
@@ -10,6 +10,7 @@ int myComp(const void *p1, const void *p2);
 // MAin Function
 int main(void)
 {
+  puts("Time challenge 1 TEST");
   /* // Challenge #1
   int i = 0;
   char str = '\0';
@@ -25,28 +26,24 @@ int main(void)
   // Challenge #2
   double vals[NUM];
   fillArray(vals, NUM);
-  
+  puts("Random List: ");
+  showArray(vals, NUM);
+  // Use of qsort();
+  qsort(vals, NUM, sizeof(double), myComp);
+  puts("Sorted List: ");
+  showArray(vals, NUM);
   /*
-  0.Use srand() with seed
-  
-  2.First line of output: number of data
-  3.Next 50 lines should be the 50 random numbers
   4.Sort an array of doubles from lowest to highest
-  5.Create a function that takes a double array and a size parameter
-  which generates some random double values
-  6.void fillArray(double ar[], int n);
-  7.Create a function that shows the array (takes a double array and size)
-  8.void showArray(const double ar[], int n);
   9.Main should contain: create array, fill it with random numbers,
   display it, sort it using qsort, display the sorted array (0-15 doubles)
   10.Write a program that displays the current time
   11.Write a program to compute the number of seconds passed since
   the beginning of the current month
   */
-  puts("Time challenge 1 TEST");
   return(0);
 }
-
+// Create a function that takes a double array and a size parameter
+// which generates some random double values
 void fillArray(double ar[], int n)
 {
   int index = 0;
@@ -54,7 +51,7 @@ void fillArray(double ar[], int n)
   for(index = 0; index < n; index++)
     ar[index] = (double)rand() / ((double) rand() + 0.1);
 }
-
+// Create a function that shows the array (takes a double array and size)
 void showArray(const double ar[], int n)
 {
   int index;
