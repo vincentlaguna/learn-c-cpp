912a0a57bf0ba7775c78557ed202cdbc2b46dff8
vincentlaguna <vincentlaguna@live.com>
2020-09-07 20:08:44 +0000

    get the day of the week your birthday was on using the time library - commit #1 - C

--- c/personal_projects/birth_day.c
+++ c/personal_projects/birth_day.c
@@ -3,5 +3,18 @@
 
 int main(void)
 {
+  const char *day[7] = {"Sunday", "Monday", "Tuesday",
+                        "Wednesday", "Thursday", "Friday", "Saturday"};
+  const char *month[12] = {"January", "February", "March", "April",
+                           "May", "June", "July", "August",
+                           "September", "October", "November", "December"};
+  const char *suffix[] = {"st", "nd", "rd", "th"};
+  enum suffindex = {st, nd, rd, th};
+  struct tm birthday {0};
+  char name[30] = {'\0'};
+  
+  printf("Enter a name: ");
+  scanf("%s", name);
+  
   return(0);
 }
