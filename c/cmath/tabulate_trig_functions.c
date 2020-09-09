7deead71cea90b09cc2b48c3bea14f4cfefd1851
vincentlaguna <vincentlaguna@live.com>
2020-09-09 22:42:08 +0000

    tabulate the values of trigonometric functions exercise - commit #1 - C

--- c/cmath/tabulate_trig_functions.c
+++ c/cmath/tabulate_trig_functions.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <math.h>
 
+void tabulate(double (*f)(double), double first, double last, double incr);
+
 int main(void)
 {
   puts("Trig TEST!");
   return(0);
+}
+
+void tabulate(double (*f)(double), double first, double last, double incr)
+{
+  
 }
