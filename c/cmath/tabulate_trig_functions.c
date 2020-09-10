b0605a9c5c127800393a3479f7edfeb968a046d9
vincentlaguna <vincentlaguna@live.com>
2020-09-10 21:11:39 +0000

    tabulate the values of trigonometric functions exercise - commit #4 math functions not linking from math.h?- C

--- c/cmath/tabulate_trig_functions.c
+++ c/cmath/tabulate_trig_functions.c
@@ -24,11 +24,23 @@ int main(void)
          "\n  --------    --------\n");
   tabulate(sin, initial, final, increment);
   
+  printf("\n     x         tan(x)"
+         "\n  --------    --------\n");
+  tabulate(tan, initial, final, increment);
   
   return(0);
 }
 
 void tabulate(double (*f)(double), double first, double last, double incr)
 {
+  double x;
+  int i, num_intervals;
   
+  num_intervals = ceil((last - first) / incr);
+  
+  for(i = 0; i < num_intervals; i++)
+  {
+    x = first + i * incr;
+    printf("%10.5f %10.5f\n", x, (*f) (x));
+  }
 }
