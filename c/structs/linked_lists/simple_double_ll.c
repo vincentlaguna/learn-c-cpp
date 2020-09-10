bb72d82b4369f536c50eb5c173afacc128cbb7aa
vincentlaguna <vincentlaguna@live.com>
2020-09-10 21:32:33 +0000

    blind try at simple double linked-list - commit #1 - C

--- c/structs/linked_lists/simple_double_ll.c
+++ c/structs/linked_lists/simple_double_ll.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct node
+{
+  char data;
+  struct node *next;
+} node_t;
+
+typedef node_t *pListNode;
+// Prototypes
+void insert();
+void insertAtStart();
+void insertAtEnd();
+char delete();
+int isEmtpy();
+void printList();
+
 int main(void)
 {
   puts("This is a linked list project - TEST");
+  
   return(0);
 }
