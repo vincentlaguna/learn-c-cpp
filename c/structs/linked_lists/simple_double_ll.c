a2217589772a63ba497514beaa115f45e50ad95e
vincentlaguna <vincentlaguna@live.com>
2020-09-12 20:07:46 +0000

    simple double linked-list - commit #2 - C

--- c/structs/linked_lists/simple_double_ll.c
+++ c/structs/linked_lists/simple_double_ll.c
@@ -6,7 +6,7 @@ typedef struct node
   char data;
   struct node *pNext;
 } node_t;
-
+// Initialize struct pointer
 typedef node_t *pListNode;
 // Prototypes
 void insert(pListNode *head, char value);
@@ -16,10 +16,24 @@ char delete(pListNode *head, char value);
 void deleteAtStart(pListNode *head)
 int isEmtpy(pListNode head);
 void printList(pListNode pCurrent);
-
+// Main functions
 int main(void)
 {
   puts("This is a linked list project - TEST");
+  pListNode head = NULL; // Because initially there no nodes
+  int choice = 0; // User's choice
+  char item = '\0'; // Item entered by the user
+  // Display the menu
+  printf("Please enter your choice:\n"
+         "1. To insert an item into the list in alphabetical order.\n"
+         "2. To insert an element at the end of the list.\n"
+         "3. To insert an item at the beginning of the list.\n"
+         "4. To delete an item from the list.\n"
+         "5. To delete an item from the beginning of the list.\n"
+         "6. To END.\n");
+  printf(":: ");
+  scanf("%d", &choice);
+  
   
   return(0);
 }
