89e8623c0a069aade458841a4c0fd2514698339d
vincentlaguna <vincentlaguna@live.com>
2020-09-13 22:43:39 +0000

    stack implementation using a linked-list - Advanced C Programming Course (Udemy) - initial commit #0 - C

---
+++ c/structs/linked_lists/stack_implementation_ex1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+  int data;
+  struct Node *pLink;
+};
+
+int main(void)
+{
+  puts("Example of a stack implementation using a linked-list - TEST");
+  
+  
+  return(0);
+}
