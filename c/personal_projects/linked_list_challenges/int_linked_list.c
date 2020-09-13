28c534477fc1bd3abd24d805058601f568f15e9b
vincentlaguna <vincentlaguna@live.com>
2020-09-13 00:14:20 +0000

    integer linked-list challenge - Advanced C Programming Course (Udemy) - commit #2 - C

--- c/personal_projects/linked_list_challenges/int_linked_list.c
+++ c/personal_projects/linked_list_challenges/int_linked_list.c
@@ -8,7 +8,7 @@ struct node
   struct node *pNext;
 };
 // Function Prototypes
-struct node* createNode(int);
+struct node* createNode(int); // Returns node pointer
 void insertAtBeginning();
 void insertAtEnd();
 void insertNodeAtPosition();
@@ -17,11 +17,30 @@ void search();
 void updateValue();
 void display();
 // Global Variables
-struct node *pNewNode, *ptr, *pPrevious, *pTemp;
+struct node *pNewNode, *pCurrent, *pPrevious, *pTemp;
 struct node *pHead = NULL, *pTail = NULL;
 // Main Function
 int main(void)
 {
   puts("Integer linked-list TEST");
+  int choice = '\0';
+  // While loop
+  while(true)
+  {
+    printf("\n----------------------------------------\n");
+    printf("\nOperations on a linked list\n");
+    printf("\n----------------------------------------\n");
+    printf("\n1. Insert node at the beginning");
+    printf("\n2. Insert node at the end");
+    printf("\n3. Insert node at a specific position");
+    printf("\n4. Delete node from any position");
+    printf("\n5. Update node value");
+    printf("\n6. Search element in the linked list");
+    printf("\n7. Display list");
+    printf("\n8. EXIT\n");
+    printf("\n----------------------------------------\n");
+    printf("\nEnter your choice: ");
+    scanf("%d", &choice);
+  }
   return(0);
 }
