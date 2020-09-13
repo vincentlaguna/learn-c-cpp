6b77ddc4b4d9170e904708a5d32c84d1545f7a61
vincentlaguna <vincentlaguna@live.com>
2020-09-13 00:26:51 +0000

    integer linked-list challenge - Advanced C Programming Course (Udemy) - commit #3 - C

--- c/personal_projects/linked_list_challenges/int_linked_list.c
+++ c/personal_projects/linked_list_challenges/int_linked_list.c
@@ -9,13 +9,13 @@ struct node
 };
 // Function Prototypes
 struct node* createNode(int); // Returns node pointer
-void insertAtBeginning();
-void insertAtEnd();
-void insertNodeAtPosition();
-void deletePosition();
-void search();
-void updateValue();
-void display();
+void insertNodeAtBeginning(void);
+void insertNodeAtEnd(void);
+void insertNodeAtPosition(void);
+void deletePosition(void);
+void search(void);
+void updateValue(void);
+void display(void);
 // Global Variables
 struct node *pNewNode, *pCurrent, *pPrevious, *pTemp;
 struct node *pHead = NULL, *pTail = NULL;
@@ -38,9 +38,76 @@ int main(void)
     printf("\n6. Search element in the linked list");
     printf("\n7. Display list");
     printf("\n8. EXIT\n");
-    printf("\n----------------------------------------\n");
+    printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
     printf("\nEnter your choice: ");
     scanf("%d", &choice);
+    // Switch case
+    switch(choice)
+    {
+      case 1:
+        insertNodeAtBeginning();
+        break;
+      case 2:
+        insertNodeAtEnd();
+        break;
+      case 3:
+        insertNodeAtPosition();
+        break;
+      case 4:
+        deletePosition();
+        break;
+      case 5:
+        updateValue();
+        break;
+      case 6:
+        search();
+        break;
+      case 7:
+        display();
+        break;
+      case 8:
+        printf(" <<< Exiting >>> \n");
+        return(0);
+        break;
+      default:
+        printf("\n...Invalid choice...\n");
+        break;
+    }
   }
   return(0);
+}
+
+void insertNodeAtBeginning(void)
+{
+  
+}
+
+void insertNodeAtEnd(void)
+{
+  
+}
+
+void insertNodeAtPosition(void)
+{
+  
+}
+
+void deletePosition(void)
+{
+  
+}
+
+void search(void)
+{
+  
+}
+
+void updateValue(void)
+{
+  
+}
+
+void display(void)
+{
+  
 }
