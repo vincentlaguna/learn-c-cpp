10fa318cdf4e2e31d61e84888d264a0f8e341524
vincentlaguna <vincentlaguna@live.com>
2020-09-18 23:06:51 +0000

    Queue implementation of a circular buffer array - Advanced C Programming Course (Udemy) - commit #8 - C

--- c/data_structures/linked_lists/circ_buff_array_ex1.c
+++ c/data_structures/linked_lists/circ_buff_array_ex1.c
@@ -78,5 +78,16 @@ void deQueue()
 
 void display()
 {
+  int i;
   
+  if(front == -1)
+    printf("Queue is empty.\n");
+  else
+  {
+    printf("Queue is: \n");
+    
+    for(i = front; i <= rear; i++)
+      printf("%d ", queue_array[i]);
+    printf("\n");
+  }
 }
