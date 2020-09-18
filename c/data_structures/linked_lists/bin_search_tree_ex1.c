79cc7c73eddb73aa72088da6929b19e34697ad27
vincentlaguna <vincentlaguna@live.com>
2020-09-18 23:21:26 +0000

    implementation of a binary search tree example - Advanced C Programming Course (Udemy) - commit #2 - C

--- c/data_structures/linked_lists/bin_search_tree_ex1.c
+++ c/data_structures/linked_lists/bin_search_tree_ex1.c
@@ -8,3 +8,21 @@ struct treeNode
   struct treeNode *pLeft; // Pointer to left subtree
   struct treeNode *pRight; // Pointer to right subtree
 };
+// Type-definitions
+typedef struct treeNode TreeNode;
+typedef TreeNode *pTreeNode;
+// Function Prototypes
+void insertNode(pTreeNode *pTree, int value);
+void inOrder(pTreeNode pTree);
+void preOrder(pTreeNode pTree);
+void postOrder(pTreeNode pTree);
+// Main Function
+int main(void)
+{
+  return(0);
+}
+// Function Definitions
+void insertNode(pTreeNode *pTree, int value);
+void inOrder(pTreeNode pTree);
+void preOrder(pTreeNode pTree);
+void postOrder(pTreeNode pTree);
