04fea8db7d1542934e3841924f80dd8c96353b53
vincentlaguna <vincentlaguna@live.com>
2020-09-18 22:33:41 +0000

    Queue implementation of a circular buffer array - Advanced C Programming Course (Udemy) - commit #2 - C

--- c/data_structures/linked_lists/circ_buff_array_ex1.c
+++ c/data_structures/linked_lists/circ_buff_array_ex1.c
@@ -1,12 +1,38 @@
 #include <stdio.h>
 
 #define MAX 50
-
+// Function Prototypes
+void enQueue();
+void deQueue();
+void display();
+// Globals
+int queue_array[MAX];
+int rear = -1;
+int front = -1;
+// Main Function
 int main(void)
 {
   puts("Basic implementation of a circular buffer array queue - TEST\n");
   
+  int choice;
+  
+  while
   
   
   return(0);
+}
+
+void enQueue()
+{
+  
+}
+
+void deQueue()
+{
+  
+}
+
+void display()
+{
+  
 }
