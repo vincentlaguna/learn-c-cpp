ceb097830bab63779361ab19375e57ffd2ec1e23
vincentlaguna <vincentlaguna@live.com>
2020-09-19 20:50:03 +0000

    inter-process communication - example of forking processes - Advanced C Programming Course (Udemy) - commit #1 - C

--- c/i_p_c/fork_proc_ex1.c
+++ c/i_p_c/fork_proc_ex1.c
@@ -4,7 +4,18 @@
 int main(void)
 {
   puts("\n<<< Example of forking processes >>>\n");
+  // Creating 1st child
+  int n1 = fork();
+  // Creating 2nd child. 1st child also executes this line
+  int n2 = fork();
   
+  if(n1 > 0 && n2 > 0)
+  {
+    printf("Parent\n");
+    printf("%d %d \n", n1, n2);
+    printf("   my id is %d \n", getpid());
+    printf("   my parent is %d \n", getppid());
+  }
   
   return(0);
 }
