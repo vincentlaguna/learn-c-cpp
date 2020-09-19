832402c710c25acebaf28b5913c7ff4392611c6c
vincentlaguna <vincentlaguna@live.com>
2020-09-19 19:00:19 +0000

    inter-process communication exercises - Advanced C Programming Course (Udemy) - commit #4 - C

--- c/i_p_c/ipc_ex1.c
+++ c/i_p_c/ipc_ex1.c
@@ -9,11 +9,14 @@
 int score = 0;
 // Function Prototypes
 void endGame();
+int catchSignal(int sig, void (*handler) (int));
 void error(char *msg);
 // Main Functions
 int main(void)
 {
   puts("\n<<< Inter-Process Communication example >>> \n");
+  
+  srand(time(0));
   
   return(0);
 }
@@ -22,6 +25,16 @@ void endGame()
 {
   printf("\nFinal Score: %d\n", score);
   exit(0);
+}
+
+int catchSignal(int sig, void (*handler) (int))
+{
+  struct sigaction action;
+  action.sa_handler = handler;
+  sigemptyset(&action.sa_mask);
+  action.sa_flags = 0;
+  
+  return sigaction(sig, &action, NULL);
 }
 
 void error(char *msg)
