b1d60ba4cbff29fc6a9facc0d0132177e69a060c
vincentlaguna <vincentlaguna@live.com>
2020-09-24 21:45:40 +0000

    passing a two-dimensional array to a function using pointers - from (C, C++, C# Programming Concepts, Black Book) - commit #1 - C

--- c/pointers/pointer_examples/pass_2darray_2func1.c
+++ c/pointers/pointer_examples/pass_2darray_2func1.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 // Function Prototypes
+void display_method_a(int *arr, int row, int col);
+void display_method_b(int (*arr)[3], int row, int col);
+void display_method_c(int arr[][3], int row, int col);
+// Main Function
 int main(void)
 {
   return(0);
+}
+// Function Definitions
+void display_method_a(int *arr, int row, int col)
+{
+  
+}
+
+void display_method_b(int (*arr)[3], int row, int col)
+{
+  
+}
+
+void display_method_c(int arr[][3], int row, int col)
+{
+  
 }
