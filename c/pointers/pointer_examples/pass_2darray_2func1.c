99fbe0c4f37349e32b21a7cd07beb7ec0b986c4f
vincentlaguna <vincentlaguna@live.com>
2020-09-25 21:38:18 +0000

    passing a two-dimensional array to a function using pointers - from (C, C++, C# Programming Concepts, Black Book) - commit #3 - C

--- c/pointers/pointer_examples/pass_2darray_2func1.c
+++ c/pointers/pointer_examples/pass_2darray_2func1.c
@@ -15,9 +15,13 @@ int main(void)
     {3, 300, 3000}
   };
   
+  printf("\n3 different methods of displaying the array -> \n\n");
   display_method_a(arr, 3, 3);
+  printf("\n");
   display_method_b(arr, 3, 3);
+  printf("\n");
   display_method_c(arr, 3, 3);
+  printf("\n");
   getchar();
   
   return(0);
@@ -39,10 +43,31 @@ void display_method_a(int *arr, int row, int col)
 
 void display_method_b(int (*arr)[3], int row, int col)
 {
+  int i, j;
+  int *ptr;
   
+  for(i = 0; i < row; i++)
+  {
+    ptr = arr + i;
+    
+    for(j = 0; j < col; j++)
+    {
+      printf("%d ", arr[i][j]);
+    }
+    printf("\n");
+  }
 }
 
 void display_method_c(int arr[][3], int row, int col)
 {
+  int i, j;
   
+  for(i = 0; i < row; i++)
+  {
+    for(j = 0; j < col; j++)
+    {
+      printf("%d ", arr[i][j]);
+    }
+    printf("\n");
+  }
 }
