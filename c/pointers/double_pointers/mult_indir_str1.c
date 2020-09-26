fb17983e17234772b65fb03431f42918ac9115ab
vincentlaguna <vincentlaguna@live.com>
2020-09-26 19:42:10 +0000

    Pointers - multiple indirection with chars - Advanced C Programming: Pointers Course (Udemy) - working commit #1 - C

--- c/pointers/double_pointers/mult_indir_str1.c
+++ c/pointers/double_pointers/mult_indir_str1.c
@@ -9,7 +9,7 @@ int main(int argc, char **argv)
   char *pChar;   // Pointer to a character
   char **ppChar; // Pointer to a pointer to a character
   
-  printf("\nMultiple indirection example with char types >>>\n");
+  printf("\nMultiple indirection example with char types >>>\n\n");
   // Initialize our string array
   words[0] = "zero";
   words[1] = "one";
@@ -21,6 +21,22 @@ int main(int argc, char **argv)
   }
   // B: a pointer to an array of strings
   // Same as a pointer to a pointer to a character
+  printf("\nNow print the chars in each string...\n\n");
+  ppChar = words; // Initializes the pointer to a pointer to a character
+  // Loop over each string
+  for(int i = 0; i < LENGTH; i++)
+  {
+    ppChar = words + i;
+    pChar = *ppChar;
+    // Process each character in a string
+    while(*pChar != 0)
+    {
+      printf("%c ", *pChar);
+      pChar += 1;
+    }
+    printf("\n");
+  }
+  printf("\n");
   
   return(0);
 }
