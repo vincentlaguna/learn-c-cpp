7fbe487948c13cc793bb0c36ca84e4153afe16cd
vincentlaguna <vincentlaguna@live.com>
2020-09-27 21:56:27 +0000

    Pointers - Generic pointer example - Advanced C Programming: Pointers Course (Udemy) - working commit #2 - C

--- c/pointers/pointer_examples/generic_pointer_ex1.c
+++ c/pointers/pointer_examples/generic_pointer_ex1.c
@@ -28,5 +28,25 @@ int main(int argc, char **argv)
     printf("%s\n", words[i]);
   }
   printf("\n");
+  /*// Data
+  pGeneric = data; // Set the generic pointer to the array
+  printf("Data array address is -> %p\n", pGeneric); // No cast needed, yet
+  // A cast is now needed to let the compiler know the type of data pointed to
+  printf("The item pointed to by pGeneric is -> %d\n", *(int*)pGeneric);
+  // The cast tells the compiler what the address type is
+  pGeneric = (int*)pGeneric + 1;
+  printf("The item pointed to by pGeneric now is -> %d\n", *(int*)pGeneric);
+  */
+  //* // Words
+  pGeneric = words; // Set the generic pointer to the array
+  printf("Words array address is -> %p\n", pGeneric); // No cast needed, yet
+  // A cast is now needed to let the compiler know the type of data pointed to
+  printf("The item pointed to by pGeneric is -> %s\n", *(char**)pGeneric);
+  // The cast tells the compiler what the address type is
+  pGeneric = (char**)pGeneric + 1;
+  printf("The item pointed to by pGeneric now is -> %s\n", *(char**)pGeneric);
+  //*/
+  printf("\n");
+  
   return(0);
 }
