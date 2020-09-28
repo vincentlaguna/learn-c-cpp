32e0456d1088c5fa89e95a858991f47dd4b82f27
vincentlaguna <vincentlaguna@live.com>
2020-09-28 21:19:03 +0000

    Pointers - memory allocation  example - Advanced C Programming: Pointers Course (Udemy) - commit #0 - C

---
+++ c/pointers/memory_allocation/adv_ptr_malloc2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+  puts("\nExample of memory allocation in a string function >>>\n");
+  
+  
+  return(0);
+}
