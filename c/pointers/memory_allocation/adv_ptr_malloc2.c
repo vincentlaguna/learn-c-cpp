4a53fa7b953ca3870d9d294e16a36229328e3c85
vincentlaguna <vincentlaguna@live.com>
2020-09-28 21:25:09 +0000

    Pointers - memory allocation  example - Advanced C Programming: Pointers Course (Udemy) - commit #1 - C

--- c/pointers/memory_allocation/adv_ptr_malloc2.c
+++ c/pointers/memory_allocation/adv_ptr_malloc2.c
@@ -2,10 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAXSTRLEN 100
+
+char *stringFunction(char *aString);
+
 int main(int argc, char **argv)
 {
   puts("\nExample of memory allocation in a string function >>>\n");
   
+  printf(stringFunction("Vincent"));
+  printf(stringFunction("Laguna"));
   
+  printf("\n");
   return(0);
+}
+
+char *stringFunction(char *aString)
+{
+  char *pString;
+  pString = (char *)malloc(sizeof(MAXSTRLEN));
+  pString[0] = 0;
+  strcat(pString, "Hello ");
+  strcat(pString, aString);
+  strcat(pString, "\n");
+  return pString;
 }
