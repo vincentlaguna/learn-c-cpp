24489a9acf4b5778b5fcb69c69bfd5561ed32199
vincentlaguna <vincentlaguna@live.com>
2020-10-01 01:09:05 +0000

    Pointers - memory allocation example 1 - Advanced C Programming: Pointers Course (Udemy) - commit #4 - C

--- c/pointers/memory_allocation/adv_ptr_malloc3.c
+++ c/pointers/memory_allocation/adv_ptr_malloc3.c
@@ -8,7 +8,16 @@ int main(int argc, char **argv)
   int stringSize;
   
   stringSize = sizeof("Hello"); // First work out size of "Hello"
-  printf("\nSize of 'Hello' is %d\n", stringSize);
+  printf("\nSize of 'Hello' is %d\n", stringSize); // Note* 6 bytes (NULL term)
   
+  pString = (char *)malloc(stringSize);
+  
+  if(pString == NULL)
+  {
+    printf("Malloc failed.\n");
+    exit(0);
+  }
+  // Now copy the string into the newly allocated memory
+  strncpy(pString, "Hello", stringSize);
   return(0);
 }
