39a6aa13476bb56c88dbe2e1ad31936411c5698d
vincentlaguna <vincentlaguna@live.com>
2020-10-14 21:36:05 +0000

    using maps - commit #2 - C++

--- c++/vectors_cpp/maps_ex1.cpp
+++ c++/vectors_cpp/maps_ex1.cpp
@@ -4,6 +4,14 @@
 
 int main(void)
 {
+  std::map<int, std::string> mapList;
+  
+  mapList.insert(std::pair<int, std::string>(1, "Hello"));
+  mapList.insert(std::pair<int, std::string>(2, "World"));
+  
+  std::cout << "Map Size: " << mapList.size() << std::endl;
+  std::cout << mapList[1] << std::endl;
+  std::cout << mapList[2] << std::endl;
   
   return(0);
 }
