b4d49bef39a7a3ebb6c7977071c8239df33a85d0
vincentlaguna <vincentlaguna@live.com>
2020-10-17 04:11:24 +0000

    class example 1 - commit #2 - C++

--- c++/oop_cpp/class_ex1.cpp
+++ c++/oop_cpp/class_ex1.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include <string>
 
+class Vehicle
+{
+protected:
+  size_t mNumOfWheels;
+  std::string mVehicleName;
+  
+  void setWheels(const size_t &wheels)
+  {
+    mNumOfWheels = wheels;
+  }
+  
+  void SetVehicleName(const)
+}
 int main(void)
 {
   return(0);
