d0f7c4339f1a032a6c0b3271caf80feb1387f858
vincentlaguna <vincentlaguna@live.com>
2020-10-17 22:17:50 +0000

    stack module implementation - abstract data types - initial commit #3 - C

--- c/data_structures/a_d_t/stack_mod/stack_1.c
+++ c/data_structures/a_d_t/stack_mod/stack_1.c
@@ -15,25 +15,29 @@ static void terminate(const char *message)
 
 void  makeEmpty(void)
 {
-  
+  top = 0;
 }
 
 bool  isEmpty(void)
 {
-  
+  return top == 0;
 }
 
 bool  isFull(void)
 {
-  
+  return top == STACK_SIZE;
 }
 
 void  push(int i)
 {
-  
+  if (isFull())
+    terminate("Error in Push: Stack is Full.\n");
+  contents[top++] = i;
 }
 
 int   pop(void)
 {
-  
+  if (isEmpty())
+    terminate("Error in Pop: Stack is Empty.\n");
+  return contents[--top];
 }
