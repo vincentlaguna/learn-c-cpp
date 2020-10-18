db7c12ea38eefa0f85189b08e2d4506ff3d34b1b
vincentlaguna <vincentlaguna@live.com>
2020-10-18 20:01:29 +0000

    stack module implementation - linked-list module - abstract data types - initial commit #8 - C

--- c/data_structures/a_d_t/stack_mod/stackADT.c
+++ c/data_structures/a_d_t/stack_mod/stackADT.c
@@ -10,3 +10,34 @@ struct stackType
   int top;
 };
 
+static void terminate(const char *message)
+{
+  printf("%s\n", message);
+  exit(EXIT_FAILURE);
+}
+
+Stack create(void)
+{
+  Stack s = malloc(sizeof(struct stackType));
+  
+  if (s == NULL)
+    terminate("Error in create: stack could not be created.\n");
+    
+  s->top = 0;
+  return s;
+}
+
+void destroy(Stack s)
+{
+  free(s);
+}
+
+void makeEmpty(Stack s)
+{
+  s->top = 0;
+}
+
+bool isEmpty(Stack s)
+{
+  return s->top == 0;
+}
