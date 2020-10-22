c28a18a487589a36e3cf775064567fe62c0de9f9
vincentlaguna <vincentlaguna@live.com>
2020-10-22 20:50:25 +0000

    rectangle struct 1 - commit #4 - C

--- c/data_structures/struct_examples/rectangle_1.c
+++ c/data_structures/struct_examples/rectangle_1.c
@@ -7,18 +7,33 @@ struct rectangle
   float width;
 }
 
-void createRectangle(struct rectangle *pR, 10.50, 20.75);
+void createRectangle(struct rectangle *pR, float h, float w);
 float getRectangleArea(struct rectangle *pR);
-void changeRectangleArea(&r, 10, 50);
+void changeRectangleArea(struct rectangle *pR, float h, float w);
 
 int main(void)
 {
   struct rectangle r;
   
-  createRectangle(&r, 10.50, 20.75);
-  getRectangleArea(&r);
-  changeRectangleArea(&r, 10, 50);
-  getRectangleArea(&r);
+  // createRectangle(&r, 10.50, 20.75);
+  // getRectangleArea(&r);
+  // changeRectangleArea(&r, 10, 50);
+  // getRectangleArea(&r);
   
   return(0);
+}
+
+void createRectangle(struct rectangle *pR, float h, float w)
+{
+  
+}
+
+float getRectangleArea(struct rectangle *pR)
+{
+  
+}
+
+void changeRectangleArea(struct rectangle *pR, float h, float w)
+{
+  
 }
