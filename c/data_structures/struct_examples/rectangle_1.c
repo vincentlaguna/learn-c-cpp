96f551b60c4acddae4708cfbd67af4ad1b2ae226
vincentlaguna <vincentlaguna@live.com>
2020-10-27 21:14:06 +0000

    rectangle struct 1 - commit #10 - C

--- c/data_structures/struct_examples/rectangle_1.c
+++ c/data_structures/struct_examples/rectangle_1.c
@@ -25,20 +25,20 @@ int main(void)
 
 void createRectangle(struct rectangle *pR, double h, double w)
 {
-  pR = malloc(sizeof(struct rectangle));
+  // pR = malloc(sizeof(struct rectangle));
   
-  if (pR == NULL)
-  {
-    fprintf(stderr, "Allocation error\n");
-    exit(1);
-  }
+  // if (pR == NULL)
+  // {
+  //   fprintf(stderr, "Allocation error\n");
+  //   exit(1);
+  // }
   pR->height = h;
   pR->width = w;
   
   printf("h = %f, w = %f\n", pR->height, pR->width);
 }
 
-void getRectangleArea(struct rectangle *pR) // returning null values!!!
+void getRectangleArea(struct rectangle *pR) // returning null values!!!... Still trying to figure out why...
 {
   printf("The area of the rectangle is -> %lf\n", pR->height); 
 }
