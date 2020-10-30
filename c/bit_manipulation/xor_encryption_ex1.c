6994d6c65d31602eb515de3d714257689b635ab1
vincentlaguna <vincentlaguna@live.com>
2020-10-30 22:57:51 +0000

    bit-wise manipulation - XOR encryption test 1 - commit #4 - C

--- c/bit_manipulation/xor_encryption_ex1.c
+++ c/bit_manipulation/xor_encryption_ex1.c
@@ -11,5 +11,14 @@ int main(void)
   int orig_char;
   int new_char;
   
+  while ((orig_char = getchar()) != EOF)
+  {
+    new_char = orig_char ^ KEY;
+    
+    if (isprint(orig_char) && isprint(new_char))
+      putchar(new_char);
+    else
+      putchar(orig_char);
+  }
   return(0);
 }
