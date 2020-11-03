336b9b5493fd7914f1f275f6473e8e9f7e93b6ea
vincentlaguna <vincentlaguna@live.com>
2020-11-03 22:45:47 +0000

    container exercise - commit #2 - C++

--- c++/oop_cpp/containers_1.cpp
+++ c++/oop_cpp/containers_1.cpp
@@ -9,7 +9,15 @@ template <typename T = int>
 
 class matrix
 {
-  
+private:
+  size_t mRows, mCols;
+  std::vector<std::vector<T> *> rows;
+
+public:
+  matrix(const size_t &rows, const size_t &cols)
+  {
+    
+  }
 };
 
 int main(void)
