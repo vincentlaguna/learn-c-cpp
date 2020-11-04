d8e47ffc4d5db3faac1a66c32015bce00e5bbac2
vincentlaguna <vincentlaguna@live.com>
2020-11-04 01:57:00 +0000

    container exercise - working commit #7 - C++

--- c++/oop_cpp/containers_1.cpp
+++ c++/oop_cpp/containers_1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 // row ->  |--|--|--|
 //         |--|--|--|
@@ -11,23 +12,23 @@ class matrix
 {
 private:
   size_t mRows, mCols;
-  std::vector<std::vector<T> *> rows;
+  std::vector<std::vector<T> *> mRowData;
 
 public:
   matrix(const size_t &rows, const size_t &cols)
   {
     mRows = rows;
     mCols = cols;
-    rows.reserve(mRows); // Reserve memory for matrix rows
+    mRowData.reserve(mRows); // Reserve memory for matrix rows
     
     for (int i = 0; i < mRows; i++)
     {
-      rows.push_back(new std::std::vector<T>(mCols));
+      mRowData.push_back(new std::vector<T>(mCols));
     }
   }
-  std::std::vector<T> &operator[] (const size_t &row_id) // Operator overloading
+  std::vector<T> &operator[] (const size_t &row_id) // Operator overloading
   {
-    return *(rows[row_id]) 
+    return *(mRowData[row_id]);
   }
 };
 
@@ -35,6 +36,6 @@ int main(void)
 {
   matrix<double> md(2, 3);
   md[1][1] = 56.4;
-  
+  std::cout << md[1][1] << std::endl;
   return(0);
 }
