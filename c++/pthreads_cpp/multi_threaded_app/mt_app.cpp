6b41c90ad02da2778c6c78cc2550e33bbae83b2b
vincentlaguna <vincentlaguna@live.com>
2020-11-05 23:24:16 +0000

    introduction to POSIX threads - example of multi-threaded application 0 - commit #1 - C++

--- c++/pthreads_cpp/multi_threaded_app/mt_app.cpp
+++ c++/pthreads_cpp/multi_threaded_app/mt_app.cpp
@@ -1,9 +1,20 @@
+#include <iostream>
 #include <thread>
 
 using namespace std;
 
-thread instance(thread_procedure)
-
+//thread instance(thread_procedure)
+void threadProc()
+{
+  for (int count = 0; count < 3; count++)
+  {
+    cout << "Message -> "
+         << count
+         << " from "
+         << this_thread::get_id()
+         << endl;
+  }
+}
 this_thread::get_id()
 
 thread::join()
