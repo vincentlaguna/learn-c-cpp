c726503505bb5c5c3673b1836b3bec1025b6dd83
vincentlaguna <vincentlaguna@live.com>
2020-11-09 22:17:06 +0000

    introduction to POSIX threads - using std::thread in an OOP fashion 0 - commit #7 - C++

--- c++/pthreads_cpp/thread_class/thread_main.cpp
+++ c++/pthreads_cpp/thread_class/thread_main.cpp
@@ -5,6 +5,21 @@ using namespace std;
 
 int main(void)
 {
+  Thread thread1, thread2, thread3;
+  
+  thread1.start();
+  thread2.start();
+  thread3.start();
+  
+  thread1.detach();
+  thread2.detach();
+  thread3.detach();
+  
+  this_thread::sleep_for(3s);
+  
+  thread1.stop();
+  thread2.stop();
+  thread3.stop();
   
   return(0);
 }
