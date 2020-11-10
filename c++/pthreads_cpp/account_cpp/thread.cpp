436dfe48552c4c594b3650a3818b8640dce669e1
vincentlaguna <vincentlaguna@live.com>
2020-11-10 22:36:30 +0000

    introduction to POSIX threads - thread synchronization - bank account exercise (refactoring thread class)- C++: From Scratch to High Performance Applications (Udemy Course) - commit #8 - C++

--- c++/pthreads_cpp/account_cpp/thread.cpp
+++ c++/pthreads_cpp/account_cpp/thread.cpp
@@ -10,3 +10,58 @@ Thread::Thread(Account *pAccount, ThreadType typeOfThread)
   threadType = typeOfThread;
 }
 
+Thread::~Thread()
+{
+  delete pThread;
+  pThread = NULL;
+}
+
+void Thread::run()
+{
+  while (1)
+  {
+    switch(threadType)
+    {
+      case DEPOSITOR:
+        
+        locker.lock(); // Added for Mutex Implementation
+        
+        cout << "Depositor: Current balance is "
+             << account.getBalance() << endl;
+             
+        pAccount->deposit(2000.00);
+        
+        cout << "Depositor: Account balance after deposit is "
+             << pAccount->getBalance() << endl;
+             
+        locker.unlock();
+        
+        this_thread::sleep_for(1s);
+        
+        break;
+        
+      case WITHDRAWER:
+      
+        locker.lock();
+        
+        cout << "Withdrawer: Current balance is "
+             << pAccount.getBalance() << endl;
+             
+        pAccount->withdraw(1000);
+        
+        cout << "Withdrawer: Account balance after withdrawing is "
+             << pAccount->getBalance() << endl;
+             
+        locker.unlock();
+        
+        this_thread::sleep_for(1s);
+        
+        break;
+    }
+  }
+}
+
+void Thread::start()
+{
+  
+}
