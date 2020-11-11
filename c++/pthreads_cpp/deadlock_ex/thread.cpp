a3d1c0c8f5d489cced2daae83bf544d66d53163b
vincentlaguna <vincentlaguna@live.com>
2020-11-11 23:08:47 +0000

    introduction to POSIX threads - thread synchronization - example of a deadlock - C++: From Scratch to High Performance Applications (Udemy Course) - commit #4 - C++

--- c++/pthreads_cpp/deadlock_ex/thread.cpp
+++ c++/pthreads_cpp/deadlock_ex/thread.cpp
@@ -20,5 +20,20 @@ Thread::~Thread()
 
 int Thread::getCount()
 {
+  cout << name << " is waiting for lock in getCount() method ..." << endl;
+  lock_guard<mutex> locker(commonLock);
+  cout << name << " has acquired lock in getCount() method ..." << endl;
   
+  return count;
+}
+
+int Thread::updateCount()
+{
+  cout << name << " is waiting for lock in updateCount() method ..." << endl;
+  lock_guard<mutex> locker(commonLock);
+  cout << name << " has acquired lock in updateCount() method ..." << endl;
+  int value - getCount();
+  count = ++value;
+  
+  return count;
 }
