340e1d85b6bbdc517a6bda1d2920254c3a5781a7
vincentlaguna <vincentlaguna@live.com>
2020-11-13 17:42:19 +0000

    Advanced C Programming: Pointers (Udemy Course) - double linked-lists 0 - commit #1 - C

--- c/data_structures/linked_lists/double_linked_list.c
+++ c/data_structures/linked_lists/double_linked_list.c
@@ -11,5 +11,21 @@ typedef struct listItem
 int main(void)
 {
   puts("Simple example of a double-linked list >>>\n");
+  
+  LISTITEM *pTemp, head;
+  
+  head.pNext = (LISTITEM*)&head;
+  head.pPrevious = (LISTITEM*)&head;
+  head.data = -1;
+  // Populate the list
+  for (int i = 0; i < 3; i++)
+  {
+    pTemp = malloc(sizeof(LISTITEM));
+    pTemp->data = i;
+    pTemp->pNext = head.pNext;
+    head->pNext = pTemp;
+    pTemp->pPrevious = &head;
+    pTemp->pNext->pPrevious = pTemp;
+  }
   return(0);
 }
